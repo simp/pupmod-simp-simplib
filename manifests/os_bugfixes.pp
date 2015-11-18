95d7b1e2b562f3e8d03f611a4d8796e22f5fdf92
Trevor Vaughan <tvaughan@onyxpoint.com>
2015-11-18 10:36:49 -0500

    (SIMP-643) Added bugfix for fixfiles
    Updated to actually include the bugfixes and provide a mechanism for
    toggling each bugfix as appropriate.
    
    SIMP-643 #comment Needed to include the bugfix code
    
    Change-Id: I2a33d29e59a734396460076649f3ed594be65f7f

--- manifests/os_bugfixes.pp
+++ manifests/os_bugfixes.pp
@@ -3,20 +3,39 @@
 # This class collects Operating System bugfixes and workarounds that do not
 # belong specifically in another module.
 #
-class simplib::os_bugfixes {
-  if $::operatingsystem in ['CentOS','RedHat'] {
-    if $::operatingsystemmajrelease == '7' {
-      file { '/etc/init.d/bugfix1049656':
-        ensure => 'file',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0755',
-        source => "puppet:///modules/${module_name}/etc/init.d/bugfix1049656"
-      }
+# === Params
+#
+# [*include_bugfix1049656*]
+# Type: Boolean
+# Default: true
+#   A workaround for the case where EL7 does not always properly run a
+#   filesystem relabel if the /.autorelabel file is present.
+#
+#   See: https://bugzilla.redhat.com/show_bug.cgi?id=1049656
+#
+class simplib::os_bugfixes (
+  $include_bugfix1049656 = true
+) {
 
-      service { 'bugfix1049656':
-        enable => true
-      }
+  validate_bool($include_bugfix1049656)
+
+  if ($include_bugfix1049656 == true and $::osfamily == 'zRedHat' and $::operatingsystemmajrelease == '7') {
+    file { '/etc/init.d/bugfix1049656':
+      ensure => 'file',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755',
+      source => "puppet:///modules/${module_name}/etc/init.d/bugfix1049656"
     }
+    service { 'bugfix1049656':
+      enable => true
+    }
+  }
+  else {
+    file { '/etc/init.d/bugfix1049656':
+      ensure  => 'absent',
+      require => Service['bugfix1049656']
+    }
+    service { 'bugfix1049656': enable => false }
   }
 }
