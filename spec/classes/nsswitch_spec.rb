4b5b7a46dde8bd5a1c684763fb39cbfc298907c3
Trevor Vaughan <tvaughan@onyxpoint.com>
2015-11-23 16:32:01 -0500

    (SIMP-631) Updated to make sssd default in EL7
    This change adds 'sss' as a option in /etc/nsswitch.conf as the default
    for EL>6. If the SSSD daemon is not running, this will have no adverse
    affects and it makes the system function properly out of the box for
    systems that do have SSSD running.
    
    Unfortunately, SSSD can have both local and remote accounts so the
    situation is not as cut and dry as it was with the 'use_ldap' variable
    in the past.
    
    Luckily, nsswitch is quite well written and will not fail on items that
    it does not recognize in the configuration file.
    
    SIMP-631 #comment Update nsswitch to use sssd by default in EL>6

--- spec/classes/nsswitch_spec.rb
+++ spec/classes/nsswitch_spec.rb
@@ -6,81 +6,154 @@ describe 'simplib::nsswitch' do
       context "on #{os}" do
         let(:facts){ facts }
         it { should compile.with_all_deps }
-        it { should create_file('/etc/nsswitch.conf').with_content(<<-EOM.gsub(/^\s+/,''))
-            passwd: files [!NOTFOUND=return] ldap
-            shadow: files [!NOTFOUND=return] ldap
-            group: files [!NOTFOUND=return] ldap
-            hosts: files dns
-            bootparams: nisplus [NOTFOUND=return] files
-            ethers: files
-            netmasks: files
-            networks: files
-            protocols: files
-            rpc: files
-            services: files
-            sudoers: files
-            netgroup: files [!NOTFOUND=return] ldap
-            publickey: nisplus
-            automount: files [!NOTFOUND=return] nisplus ldap
-            aliases: files nisplus
-            EOM
+        it {
+          if facts[:osfamily] == 'RedHat'
+            if facts[:operatingsystemmajrelease] < '7'
+              should create_file('/etc/nsswitch.conf').with_content(<<-EOM.gsub(/^\s+/,''))
+                passwd: files
+                shadow: files
+                group: files
+                hosts: files dns
+                bootparams: nisplus [NOTFOUND=return] files
+                ethers: files
+                netmasks: files
+                networks: files
+                protocols: files
+                rpc: files
+                services: files
+                sudoers: files
+                netgroup: files
+                publickey: nisplus
+                automount: files nisplus
+                aliases: files nisplus
+                EOM
+            else
+              should create_file('/etc/nsswitch.conf').with_content(<<-EOM.gsub(/^\s+/,''))
+                passwd: files [!NOTFOUND=return] sss
+                shadow: files [!NOTFOUND=return] sss
+                group: files [!NOTFOUND=return] sss
+                hosts: files dns
+                bootparams: nisplus [NOTFOUND=return] files
+                ethers: files
+                netmasks: files
+                networks: files
+                protocols: files
+                rpc: files
+                services: files
+                sudoers: files [!NOTFOUND=return] sss
+                netgroup: files [!NOTFOUND=return] sss
+                publickey: nisplus
+                automount: files nisplus
+                aliases: files nisplus
+                EOM
+            end
+          end
         }
 
         context 'with_initgroups' do
           let(:params){{ :initgroups => ['files'] }}
-
-          it { should create_file('/etc/nsswitch.conf').with_content(<<-EOM.gsub(/^\s+/,''))
-            passwd: files [!NOTFOUND=return] ldap
-            shadow: files [!NOTFOUND=return] ldap
-            group: files [!NOTFOUND=return] ldap
-            initgroups: files
-            hosts: files dns
-            bootparams: nisplus [NOTFOUND=return] files
-            ethers: files
-            netmasks: files
-            networks: files
-            protocols: files
-            rpc: files
-            services: files
-            sudoers: files
-            netgroup: files [!NOTFOUND=return] ldap
-            publickey: nisplus
-            automount: files [!NOTFOUND=return] nisplus ldap
-            aliases: files nisplus
-            EOM
+  
+          it {
+            if facts[:osfamily] == 'RedHat'
+              if facts[:operatingsystemmajrelease] < '7'
+                should create_file('/etc/nsswitch.conf').with_content(<<-EOM.gsub(/^\s+/,''))
+                  passwd: files
+                  shadow: files
+                  group: files
+                  initgroups: files
+                  hosts: files dns
+                  bootparams: nisplus [NOTFOUND=return] files
+                  ethers: files
+                  netmasks: files
+                  networks: files
+                  protocols: files
+                  rpc: files
+                  services: files
+                  sudoers: files
+                  netgroup: files
+                  publickey: nisplus
+                  automount: files nisplus
+                  aliases: files nisplus
+                  EOM
+              else
+                should create_file('/etc/nsswitch.conf').with_content(<<-EOM.gsub(/^\s+/,''))
+                  passwd: files [!NOTFOUND=return] sss
+                  shadow: files [!NOTFOUND=return] sss
+                  group: files [!NOTFOUND=return] sss
+                  initgroups: files
+                  hosts: files dns
+                  bootparams: nisplus [NOTFOUND=return] files
+                  ethers: files
+                  netmasks: files
+                  networks: files
+                  protocols: files
+                  rpc: files
+                  services: files
+                  sudoers: files [!NOTFOUND=return] sss
+                  netgroup: files [!NOTFOUND=return] sss
+                  publickey: nisplus
+                  automount: files nisplus
+                  aliases: files nisplus
+                  EOM
+              end
+            end
           }
         end
-
+  
         context 'with_no_ldap' do
           let(:params){{ :use_ldap => false }}
-
-          it { should create_file('/etc/nsswitch.conf').with_content(<<-EOM.gsub(/^\s+/,''))
-            passwd: files
-            shadow: files
-            group: files
-            hosts: files dns
-            bootparams: nisplus [NOTFOUND=return] files
-            ethers: files
-            netmasks: files
-            networks: files
-            protocols: files
-            rpc: files
-            services: files
-            sudoers: files
-            netgroup: files
-            publickey: nisplus
-            automount: files nisplus
-            aliases: files nisplus
-            EOM
+  
+          it {
+            if facts[:osfamily] == 'RedHat'
+              if facts[:operatingsystemmajrelease] < '7'
+                should create_file('/etc/nsswitch.conf').with_content(<<-EOM.gsub(/^\s+/,''))
+                  passwd: files
+                  shadow: files
+                  group: files
+                  hosts: files dns
+                  bootparams: nisplus [NOTFOUND=return] files
+                  ethers: files
+                  netmasks: files
+                  networks: files
+                  protocols: files
+                  rpc: files
+                  services: files
+                  sudoers: files
+                  netgroup: files
+                  publickey: nisplus
+                  automount: files nisplus
+                  aliases: files nisplus
+                  EOM
+              else
+                should create_file('/etc/nsswitch.conf').with_content(<<-EOM.gsub(/^\s+/,''))
+                  passwd: files [!NOTFOUND=return] sss
+                  shadow: files [!NOTFOUND=return] sss
+                  group: files [!NOTFOUND=return] sss
+                  hosts: files dns
+                  bootparams: nisplus [NOTFOUND=return] files
+                  ethers: files
+                  netmasks: files
+                  networks: files
+                  protocols: files
+                  rpc: files
+                  services: files
+                  sudoers: files [!NOTFOUND=return] sss
+                  netgroup: files [!NOTFOUND=return] sss
+                  publickey: nisplus
+                  automount: files nisplus
+                  aliases: files nisplus
+                  EOM
+              end
+            end
           }
         end
-
+  
         context 'with_sssd' do
           let(:params){{
             :use_ldap => false,
             :use_sssd => true
           }}
-
+  
           it { should create_file('/etc/nsswitch.conf').with_content(<<-EOM.gsub(/^\s+/,''))
             passwd: files [!NOTFOUND=return] sss
             shadow: files [!NOTFOUND=return] sss
@@ -101,13 +174,13 @@ describe 'simplib::nsswitch' do
             EOM
           }
         end
-
+  
         context 'with_sssd_and_ldap' do
           let(:params){{
             :use_ldap => true,
             :use_sssd => true
           }}
-
+  
           it { should create_file('/etc/nsswitch.conf').with_content(<<-EOM.gsub(/^\s+/,''))
             passwd: files [!NOTFOUND=return] sss ldap
             shadow: files [!NOTFOUND=return] sss ldap
