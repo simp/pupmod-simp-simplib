e1d3b6b974346bf7bc0da5f544c119c396097a66
Chris Tessmer <chris.tessmer@onyxpoint.com>
2016-01-07 23:36:22 -0500

    (SIMP-716) Confine Linux facts that break Windows
    Before this commit, the custom facts `boot_dir_uuid`, `defaultgateway`,
    `defaultgatewayiface`, `runlevel`, `shmall` would report errors during
    agent runs on Windows clients.
    
    This patch confines the problem facts to the `:kernel` type `linux`.
    
    SIMP-716 #close #comment Confined Linux facts that break Windows agents
    
    Change-Id: I981de83002ac57dbff6a01eac8371e1e41b300a0

--- lib/facter/defaultgateway.rb
+++ lib/facter/defaultgateway.rb
@@ -3,13 +3,15 @@
 # Return the default gateway of the system
 #
 Facter.add(:defaultgateway) do
-    setcode do
-        netstat = %x{/bin/netstat -rn}
-        gw = "unknown"
-        if netstat =~ /^0\.0\.0\.0\s+(.*)/ then
-            gw = $1.split(/\s+/).first
-        end
+  confine :kernel => 'Linux'
 
-        gw
+  setcode do
+    netstat = %x{/bin/netstat -rn}
+    gw = "unknown"
+    if netstat =~ /^0\.0\.0\.0\s+(.*)/ then
+      gw = $1.split(/\s+/).first
     end
+
+    gw
+  end
 end
--- lib/facter/defaultgatewayiface.rb
+++ lib/facter/defaultgatewayiface.rb
@@ -3,13 +3,15 @@
 # Return the default gw interface of the system.
 #
 Facter.add(:defaultgatewayiface) do
-    setcode do
-        netstat = %x{/bin/netstat -rn}
-        gw_iface = "unknown"
-        if netstat =~ /^0\.0\.0\.0\s+(.*)/ then
-            gw_iface = $1.split(/\s+/).last
-        end
+  setcode do
+    confine :kernel => 'Linux'
 
-        gw_iface
+    netstat = %x{/bin/netstat -rn}
+    gw_iface = "unknown"
+    if netstat =~ /^0\.0\.0\.0\s+(.*)/ then
+      gw_iface = $1.split(/\s+/).last
     end
+
+    gw_iface
+  end
 end
