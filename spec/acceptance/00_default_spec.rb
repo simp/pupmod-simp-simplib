2d98103a2b12b6fbe964c69fda99132bc1dae56c
Trevor Vaughan <tvaughan@onyxpoint.com>
2016-07-21 13:39:00 -0400

    (SIMP-1275) Validate compliance_markup mapping
    * Ported to new RPM build structure
    * Validated compliance_markup dependencies
    * Fixed the acceptance tests
    
    SIMP-1275 #comment validated compliance_markup mapping
    
    Change-Id: I5c533412f31af75243ddab966851226b136052ad

---
+++ spec/acceptance/00_default_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper_acceptance'
+
+test_name 'simplib class'
+
+describe 'simplib class' do
+  let(:manifest) {
+    <<-EOS
+      include 'simplib'
+    EOS
+  }
+
+  hosts.each do |host|
+    context 'default parameters' do
+
+      it 'should work with no errors' do
+        apply_manifest_on(host, manifest, :catch_failures => true)
+      end
+
+      it 'should be idempotent' do
+        apply_manifest_on(host, manifest, :catch_changes => true)
+      end
+    end
+  end
+end
