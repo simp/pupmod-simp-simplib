b638d890f1c3b685a75668b046efe0c0e8190596
Trevor Vaughan <tvaughan@onyxpoint.com>
2016-11-16 17:52:19 -0500

    (SIMP-2038) Fix issues with puppet_settings
    The puppet_settings fact had several issues.
    
    This rewrite corrects these issues with much cleaner code.
    
    SIMP-2038 #close
    SIMP-2037 #close

--- lib/facter/puppet_settings.rb
+++ lib/facter/puppet_settings.rb
@@ -5,40 +5,19 @@
 #
 # Each entry is under a hash entry of its associated section
 
-begin
-  require 'facter/util/puppet_settings'
-rescue LoadError => e
-  # puppet apply does not add module lib directories to the $LOAD_PATH (See
-  # #4248). It should (in the future) but for the time being we need to be
-  # defensive which is what this rescue block is doing.
-  rb_file = File.join(File.dirname(__FILE__), 'util', 'puppet_settings.rb')
-  load rb_file if File.exists?(rb_file) or raise e
-end
-
 Facter.add(:puppet_settings) do
   setcode do
     retval = {}
-    # This will be nil if Puppet is not available.
-    Facter::Util::PuppetSettings.with_puppet do
-      Puppet.settings.each do |setting|
-        key, params = setting
 
-        # Apparently, accessing these is deprecated
-        deprecated_access = [
-          :req_bits,
-          :ignorecache,
-          :configtimeout
-        ]
-        next if deprecated_access.include?(key)
+    if Object.const_defined?('Puppet')
+      puppet_settings = Hash[Puppet.settings.map{|k,v| [k,v]}]
 
-        # This is nonsensical
-        next if (key == :name) && (params.section == :main)
-
-        next if (!Puppet[key] || (Puppet[key].respond_to?(:empty?) && Puppet[key].empty?))
+      puppet_settings.each_pair do |name, obj|
+        next if obj.deprecated?
 
         # For older versions of Facter, no hash keys/values can be symbols
-        retval[params.section.to_s] ||= {}
-        retval[params.section.to_s][params.name.to_s] = Puppet[key].to_s
+        retval[obj.section.to_s] ||= {}
+        retval[obj.section.to_s][name.to_s] = obj.value.to_s
       end
     end
 
