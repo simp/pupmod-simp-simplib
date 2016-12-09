a5db3a15f246e6bbdd0cf479591b7a07ec813275
Nick Miller <nick.miller@onyxpoint.com>
2016-12-09 14:36:08 -0500

    (SIMP-2312) Remove manifests and templates
    NOTE: THIS MODULE CONTAINS NO MORE CLASSES OR MANIFESTS
    
    All of this content has been put elsewhere. SIMP-1679 has details.
    
    SIMP-1679 #comment manifests and templates removed
    SIMP-2312 #close

--- spec/classes/prelink_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe 'simplib::prelink' do
-
-  it { is_expected.to compile.with_all_deps }
-  it { is_expected.to contain_file('/etc/sysconfig/prelink').with_content(/PRELINKING=no/) }
-
-end
