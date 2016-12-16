d40926e9009cef04d643a1b815445378cbc22d45
Lucas Yamanishi <lamawithonel@users.noreply.github.com>
2016-12-16 17:26:19 -0500

    Fix SystemPort test name

--- spec/unit/data_types/systemport_spec.rb
+++ spec/unit/data_types/systemport_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 if Puppet.version.to_f >= 4.5
-  describe 'simplib_privilegedport', type: :class do
+  describe 'Simplib::SystemPort', type: :class do
     describe 'valid handling' do
       let(:pre_condition) {%(
         class #{class_name} (
