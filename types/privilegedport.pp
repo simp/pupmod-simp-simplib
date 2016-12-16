1e8c0433e488f4dac3bf28b781aa41a306791ed5
Trevor Vaughan <tvaughan@onyxpoint.com>
2016-12-16 12:06:14 -0500

    (SIMP-1695) Add set of Port Data Types
    This adds the following Data Types:
      * Port (any of the below)
      * RandomPort (0)
      * PrivilegedPort (1-1024)
      * UnprivilegedPort (1025-65535)
    
    SIMP-1695 #comment Required for Puppet 4 refactor

---
+++ types/privilegedport.pp
@@ -0,0 +1,2 @@
+# Corresponds to the usual privileged port range
+type Simplib::PrivilegedPort = Integer[1,1024]
---
+++ types/unprivilegedport.pp
@@ -0,0 +1,2 @@
+# Corresponds to the unprivileged port range
+type Simplib::UnprivilegedPort = Integer[1025,65535]
