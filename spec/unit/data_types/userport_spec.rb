8c76fc26e1e6282886658a7a40d214753a1b3e62
Lucas Yamanishi <lamawithonel@users.noreply.github.com>
2016-12-16 17:26:54 -0500

    Fix UserPort test name

--- spec/unit/data_types/userport_spec.rb
+++ spec/unit/data_types/userport_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 if Puppet.version.to_f >= 4.5
-  describe 'simplib_unprivilegedport', type: :class do
+  describe 'Simplib::UserPort', type: :class do
     describe 'valid handling' do
       let(:pre_condition) {%(
         class #{class_name} (
