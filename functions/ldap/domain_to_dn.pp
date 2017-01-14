6b4e4237244c563bd9b15e2497aa907745a055fb
Trevor Vaughan <trevor-vaughan@users.noreply.github.com>
2017-01-14 09:08:06 -0500

    (SIMP-2180) Added simplib::ldap::domain_to_dn (#77)
    This is to support the OpenLDAP rewrite as well as any other modules
    that may need a reasonable base DN
    
    SIMP-2180 #comment Added function for converting from domain to DN

---
+++ functions/ldap/domain_to_dn.pp
@@ -0,0 +1,6 @@
+# Provides a reasonable LDAP Base DN as generated from the ``domain`` fact
+function simplib::ldap::domain_to_dn (
+  String $domain = $facts['domain']
+) {
+  join(split($domain,'\.').map |$x| { "dc=${x}" }, ',')
+}
