9bf8ae12df6b76d276ed2b98c295037d3c780371
Nick Miller <nick.miller@onyxpoint.com>
2017-01-30 19:56:38 -0500

    (SIMP-2547) Add fips_ciphers fact (#79)
    (SIMP-2547) Add fips_ciphers fact
    
    The fips_ciphers fact returns the available OpenSSL FIPS ciphers on the
    system. This will be used as the default ciphers set for OpenLDAP,
    because it doens't like the FIPS shortcut.
    
    SIMP-2546 #comment added the fact
    SIMP-2547 #close

---
+++ lib/facter/fips_ciphers.rb
@@ -0,0 +1,9 @@
+# List available FIPS-compatible OpenSSL ciphers on the system
+# Returns: Array[String]
+Facter.add('fips_ciphers') do
+  confine :kernel => 'Linux'
+
+  setcode do
+    Facter::Core::Execution.exec('openssl ciphers FIPS:!LOW').split(':')
+  end
+end
