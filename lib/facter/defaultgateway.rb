c998271bbed120f549e5c6ceea2a468edc6dff3d
enemsick <lnemsick.simp@gmail.com>
2017-04-12 13:11:19 +0000

    (SIMP-1486) Remove netstat from defaultgateway/defaultgatewayiface facts
    Implemented defaultgateway and defaultgatewayiface facts using
    standard ip command to eliminate dependency on net-tools package.
    
    SIMP-1486 #close

--- lib/facter/defaultgateway.rb
+++ lib/facter/defaultgateway.rb
@@ -1,17 +1,23 @@
 # _Description_
 #
-# Return the default gateway of the system
+# Return the default IPv4 gateway of the system
 #
 Facter.add(:defaultgateway) do
   confine :kernel => 'Linux'
 
   setcode do
-    netstat = %x{/bin/netstat -rn}
     gw = "unknown"
-    if netstat =~ /^0\.0\.0\.0\s+(.*)/ then
-      gw = $1.split(/\s+/).first
+    ip_cmd = Facter::Util::Resolution.which('ip')
+    if ip_cmd
+      route_lines = Facter::Core::Execution.exec("#{ip_cmd} route").split("\n")
+      gw_lines = route_lines.delete_if { |line| !line.match(/^default\s+via\s+/) }
+      unless gw_lines.empty?
+        match = gw_lines.last.match(/default\s+via\s+([0-9\.]*)\s+dev\s+/)
+        if match
+          gw = match[1]
+        end
+      end
     end
-
     gw
   end
 end
--- lib/facter/defaultgatewayiface.rb
+++ lib/facter/defaultgatewayiface.rb
@@ -1,17 +1,23 @@
 # _Description_
 #
-# Return the default gw interface of the system.
+# Return the default IPv4 gw interface of the system.
 #
 Facter.add(:defaultgatewayiface) do
   confine :kernel => 'Linux'
+
   setcode do
-
-    netstat = %x{/bin/netstat -rn}
     gw_iface = "unknown"
-    if netstat =~ /^0\.0\.0\.0\s+(.*)/ then
-      gw_iface = $1.split(/\s+/).last
+    ip_cmd = Facter::Util::Resolution.which('ip')
+    if ip_cmd
+      route_lines = Facter::Core::Execution.exec("#{ip_cmd} route").split("\n")
+      gw_lines = route_lines.delete_if { |line| !line.match(/^default\s+via\s+/) }
+      unless gw_lines.empty?
+        match = gw_lines.last.match(/\s+dev\s+(\S+)/)
+        if match
+          gw_iface = match[1]
+        end
+      end
     end
-
     gw_iface
   end
 end
