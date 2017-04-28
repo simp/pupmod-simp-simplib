05a11b287e085d1c45b8ef02a66cd5b623e7d08f
Trevor Vaughan <tvaughan@onyxpoint.com>
2017-04-27 21:09:41 -0400

    (SIMP-3115) Added wrapper stages to simplib
    There have been several situations where various SIMP components really
    needed to be wrapped in a Puppet stage to protect the rest of the
    system from a particularly aggressive action (usually one that requires
    a reboot).
    
    This change adds a couple of SIMP stages that wrap the stdlib stages to
    provide a buffer zone for SIMP-specific activities. This is not a
    perfect solution but it will work better than just trying to tag onto
    the existing stages that don't actually map to our use cases.
    
    SIMP-3115 #comment Added buffer stages for module locking

---
+++ manifests/stages.pp
@@ -0,0 +1,19 @@
+# This class expands on the Puppet Stdlib stages to add a few levels that we
+# found necessary when developing various SIMP modules that had global
+# ramifications.
+#
+# Primarily, we wanted to ensure that anyone using the stdlib stages was not
+# tripped up by any of our modules that may enable, or disable, various system,
+# components; particularly ones that require a reboot.
+#
+# Added Stages:
+#
+#   * ``simp_prep`` -> Comes before stdlib's ``setup``
+#   * ``simp_finalize`` -> Comes after stdlib's ``deploy``
+#
+class simplib::stages {
+  include stdlib::stages
+
+  stage { 'simp_prep': before      => Stage['setup'] }
+  stage { 'simp_finalize': require => Stage['deploy'] }
+}
