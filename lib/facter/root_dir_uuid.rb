5824628acf1a29176fc8d908361f2a4a52cca1b3
Trevor Vaughan <trevor-vaughan@users.noreply.github.com>
2017-05-10 10:47:07 -0400

    (SIMP-3138) Added new root_dir_uuid fact (#93)
    - Cleaned up the boot_dir_uuid fact
    - Reverted the version number for the module since the last release was
      3.2.0
    
    SIMP-3138 #comment Added supporting root_dir_uuid fact for the fips module

--- lib/facter/root_dir_uuid.rb
+++ lib/facter/root_dir_uuid.rb
@@ -1,12 +1,12 @@
-# Return the UUID of the partition holding the /boot directory
-Facter.add('boot_dir_uuid') do
+# Return the UUID of the partition holding the / directory
+Facter.add('root_dir_uuid') do
   confine :kernel => 'Linux'
 
   setcode do
     df_cmd = Facter::Util::Resolution.which('df')
     blkid_cmd = Facter::Util::Resolution.which('blkid')
 
-    partition = Facter::Core::Execution.exec("#{df_cmd} -P /boot").strip.split("\n").last.split(' ').first
+    partition = Facter::Core::Execution.exec("#{df_cmd} -P /").strip.split("\n").last.split(' ').first
 
     uuid = Facter::Core::Execution.exec("#{blkid_cmd} -s UUID -o value #{partition}").strip
 
