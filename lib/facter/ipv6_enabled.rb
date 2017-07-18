62119c5904fc35c3bd98eb299c3143cb39723076
Dylan Cochran <dylan.cochran@onyxpoint.com>
2017-07-18 15:58:15 -0400

    (SIMP-3447) ipv6_enabled is not confined properly
    ipv6_enabled runs on all kernels, when the sysctl in question
    is linux specific. This causes spurious errors on non-linux
    systems
    
    SIMP-3447 #close SIMP-1388 #close

--- lib/facter/ipv6_enabled.rb
+++ lib/facter/ipv6_enabled.rb
@@ -4,9 +4,10 @@
 # Known to work on 2.4.6 kernels.
 #
 Facter.add("ipv6_enabled") do
+  confine :kernel => 'Linux'
   setcode do
     retval = false
-    ipv6_enabled = Facter::Core::Execution.exec('/sbin/sysctl -n -e net.ipv6.conf.all.disable_ipv6')
+    ipv6_enabled = Facter::Core::Execution.exec('/sbin/sysctl -n -e net.ipv6.conf.all.disable_ipv6 2>/dev/null')
 
     # we have observed this exec non-deterministically populate $? with
     # nil, although the exec succeeds.  This will happen with %x, ``, or
