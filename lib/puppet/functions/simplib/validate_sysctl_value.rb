c1965d6bcfd36fc92621ef53360b1f9fe154b375
lnemsick-simp <lnemsick.simp@gmail.com>
2017-10-26 18:51:54 +0000

    (SIMP-3914) Convert validate_sysctl_value and write test
    SIMP-3914 #close

---
+++ lib/puppet/functions/simplib/validate_sysctl_value.rb
@@ -0,0 +1,40 @@
+#    Validate that the passed value is correct for the passed `sysctl` key.
+#    If a key is not known, assumes the value is valid.
+Puppet::Functions.create_function(:'simplib::validate_sysctl_value') do
+
+  # @param key sysctl setting whose value is to be validated
+  # @param value Value to be validated
+  # @return [Nil]
+  # @raise RuntimeError upon validation failure
+  # @example Passing validation
+  #   validate_sysctl_value('kernel.core_pattern','/var/core/%u_%g_%p_%t_%h_%e.core')
+
+  dispatch :validate_sysctl_value do
+    required_param 'String',:key
+    required_param 'String',:value
+  end
+
+  def validate_sysctl_value(key, value)
+
+    key_method_name = key.to_s.gsub('.','__')
+
+    self.send(key_method_name,value) if self.respond_to?(key_method_name)
+  end
+
+  # Below are the recognized validation methods
+
+  def kernel__core_pattern(value)
+    method = 'kernel.core_pattern'
+
+    if value.length > 128
+      fail("simplib::validate_sysctl_value(): Values for #{method} must be less than 129 characters")
+    end
+
+    if value =~ /\|\s*(\S*)/
+      require 'puppet/util'
+      unless  Puppet::Util.absolute_path?($1, :posix)
+        fail("simplib::validate_sysctl_value(): Piped commands for #{method} must have an absolute path")
+      end
+    end
+  end
+end
