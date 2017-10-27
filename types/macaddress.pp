10b9e11862644d65315f0feb22906f4ecf8afaea
lnemsick-simp <lnemsick.simp@gmail.com>
2017-10-27 13:15:41 +0000

    Get rid of commas in regexes

--- types/macaddress.pp
+++ types/macaddress.pp
@@ -1,2 +1,2 @@
 # Matches MAC addresses
-type Simplib::Macaddress = Pattern['^[0-9,a-f,A-F]{1,2}:[0-9,a-f,A-F]{1,2}:[0-9,a-f,A-F]{1,2}:[0-9,a-f,A-F]{1,2}:[0-9,a-f,A-F]{1,2}:[0-9,a-f,A-F]{1,2}$']
+type Simplib::Macaddress = Pattern['^[0-9a-fA-F]{1,2}:[0-9a-fA-F]{1,2}:[0-9a-fA-F]{1,2}:[0-9a-fA-F]{1,2}:[0-9a-fA-F]{1,2}:[0-9a-fA-F]{1,2}$']
