2e1798b255662fdeb61989af72d25a5b9b401daf
Chris Tessmer <chris.tessmer@onyxpoint.com>
2017-12-08 14:56:56 +0000

    Remove Puppet 4.5 check

--- spec/unit/data_types/domainlist_spec.rb
+++ spec/unit/data_types/domainlist_spec.rb
@@ -1,55 +1,53 @@
 require 'spec_helper'
 
-if Gem::Version.new( Puppet.version ) >= Gem::Version.new( 4.5 )
-  test_data_type = 'Simplib::Domainlist'
-  plural_item    = 'Arrays of DNS domain names'
-  singular_item  = 'Array of DNS domain names'
+test_data_type = 'Simplib::Domainlist'
+plural_item    = 'Arrays of DNS domain names'
+singular_item  = 'Array of DNS domain names'
 
-  valid_data = [
-    ['test.com','test','t','0.t-t.0.t','0-0.0-0'],
-    ['0-0'],
-  ]
+valid_data = [
+  ['test.com','test','t','0.t-t.0.t','0-0.0-0'],
+  ['0-0'],
+]
 
-  invalid_data = [
-    ['test.com','test','t','test-.com'],
-    ['-test'],
-    ['t.t.t.t.0', 'test.com'],
-  ]
+invalid_data = [
+  ['test.com','test','t','test-.com'],
+  ['-test'],
+  ['t.t.t.t.0', 'test.com'],
+]
 
-  describe test_data_type, type: :class do
-    describe 'valid handling' do
-      let(:pre_condition) {%(
-        class #{class_name} (
-          #{test_data_type} $param
-        ){ }
+describe test_data_type, type: :class do
+  describe 'valid handling' do
+    let(:pre_condition) {%(
+      class #{class_name} (
+        #{test_data_type} $param
+      ){ }
 
-        class { '#{class_name}':
-          param => #{param}
-        }
-      )}
+      class { '#{class_name}':
+        param => #{param}
+      }
+    )}
 
-      context "with valid #{plural_item}" do
-        valid_data.each do |data|
-          context "with #{singular_item} '#{data}'" do
-            let(:param){ data.is_a?(String) ? "'#{data}'" : data }
+    context "with valid #{plural_item}" do
+      valid_data.each do |data|
+        context "with #{singular_item} '#{data}'" do
+          let(:param){ data.is_a?(String) ? "'#{data}'" : data }
 
-            it 'should compile' do
-              is_expected.to compile
-            end
+          it 'should compile' do
+            is_expected.to compile
           end
         end
       end
+    end
 
-      context "with invalid #{plural_item}" do
-        invalid_data.each do |data|
-          context "with #{singular_item} '#{data}'" do
-            let(:param){
-              data.is_a?(String) ? "'#{data}'" : data
-            }
+    context "with invalid #{plural_item}" do
+      invalid_data.each do |data|
+        context "with #{singular_item} '#{data}'" do
+          let(:param){
+            data.is_a?(String) ? "'#{data}'" : data
+          }
 
-            it 'should fail to compile' do
-              is_expected.to compile.and_raise_error(/parameter 'param'( index \d+)? expects/)
-            end
+          it 'should fail to compile' do
+            is_expected.to compile.and_raise_error(/parameter 'param'( index \d+)? expects/)
           end
         end
       end
