7712c603546badd801cc657dc3913ab1bfd07ca1
Trevor Vaughan <tvaughan@onyxpoint.com>
2018-01-22 13:05:18 -0500

    working acceptance test

--- lib/facter/ipa.rb
+++ lib/facter/ipa.rb
@@ -1,23 +1,16 @@
 # _Description_
 #
 # Return a hash of IPA information:
-# * status:
-#   - 'joined' when the host is able to pull both the domain and IPA
-#      server information from an IPA server
-#   - 'unknown' when the host is not able to pull both the domain
-#     and server information from an IPA server.  This status can
-#     occur when connectivity to the IPA server is down or the
-#     host has been unprovisioned at the IPA server and thus is
-#     no longer joined to the configured IPA domain.
-# * domain: The IPA domain or nil, if the host is unable to pull the
-#   domain information from the IPA server
-# * server: The IPA server to which this host is connected or nil,
-#   if the host is unable to pull the server information from the
-#   IPA server
-# * default_domain:  The configured IPA domain from the domain setting
-#     in /etc/ipa/default.conf.
-# * default_server:  The nominal, configured IPA server extracted from
-#     the xmlrpc_uri setting in /etc/ipa/default.conf.
+# * connectd: Boolean value based on whether or not an IPA server could be reached
+#
+# * domain: The IPA domain for which this host is configured
+#
+# * server: The IPA server for which this host is configured
+#
+# * realm: The Kerberos realm for the host
+#
+# * tls_ca_cert: The location of the IPA server's TLS CA Certificate if the
+#     system is an IPA server
 #
 Facter.add(:ipa) do
   confine :kernel => 'Linux'
@@ -29,65 +22,64 @@ Facter.add(:ipa) do
   confine { ipa }
 
   # This file is only present if the host has, at some time,
-  # been joined to an IPA domain.  Its presence, however, is
-  # insufficient to tell us if the host is currently joined.
-  # A host can be unprovisioned by the IPA server and still
-  # retain this file.
+  # been joined to an IPA domain.
   confine { File.exist?('/etc/ipa/default.conf') }
 
   setcode do
-    ipa_fact = {}
-    defaults = IO.readlines('/etc/ipa/default.conf')
-    defaults.each do |line|
-      domain_match = line.match(/^domain\s*=\s*(\S*)/)
-      if domain_match
-        ipa_fact['default_domain'] = domain_match[1]
-      end
+    needed_keys = [
+      'domain',
+      'server',
+      'realm',
+      'basedn',
+      'tls_ca_cert'
+    ]
 
-      server_match = line.match(/^xmlrpc_uri\s*=\s*http[s]?:\/\/(\S*)\/ipa\/xml/)
-      if server_match
-        ipa_fact['default_server'] = server_match[1]
-      end
-      break if ipa_fact['default_domain'] and ipa_fact['default_server']
+    file_defaults = File.read('/etc/ipa/default.conf').lines.
+      map(&:strip).
+      map{ |x|
+        x.split(/\s*=\s*(.*)/)
+      }.delete_if{|x|
+        x.size < 2
+      }.flatten
+
+    defaults = Hash[*file_defaults]
+
+    defaults.delete_if { |k,v| !needed_keys.include?(k) }
+
+    # We won't know if we are connected to a server until later
+    defaults['connected'] = false
+
+    # Grab the necessary information from 'ipa env'
+    ipa_response = Facter::Core::Execution.exec("#{ipa} env #{needed_keys.join(' ')}")
+
+    unless $?.success?
+      # Obtain host Kerberos token so we can use IPA API
+      kinit_msg = Facter::Core::Execution.exec("#{kinit} -k 2>&1")
+      ipa_response = Facter::Core::Execution.exec("#{ipa} env #{needed_keys.join(' ')}")
     end
 
-    # Obtain host Kerberos token so we can use IPA API
-    kinit_msg = Facter::Core::Execution.exec("#{kinit} -k 2>&1")
-    if kinit_msg.nil? or !kinit_msg.strip.empty?
-      # Only messages emitted are error messages
-      ipa_fact.merge!({ 'status' => 'unknown', 'domain' => nil, 'server' => nil })
+    if ipa_response.strip.empty?
+      ipa_response = {}
     else
-      # Use IPA API to determine this host's IPA server
-      #
-      # TODO: Just use 'ipa env server', once support for el6 is dropped.
-      #       Unfortunately, this is not available on el6.
-      host = Facter::Core::Execution.exec("#{ipa} env host")
-      host = host.strip.split('host:')[1] unless host.nil?
-      unless host.nil? or host.strip.empty?
-        # 'ipa host-show' will prompt if no host is passed in...
-        host_info = Facter::Core::Execution.exec("#{ipa} host-show #{host.strip}").to_s
-        managed_line = host_info.split("\n").delete_if {
-          |line| !line.include?('Managed by:')
-        }
-        server = managed_line.empty? ? nil : managed_line[0].strip.split('Managed by:')[1]
-        server = nil if (server and server.strip.empty?)
-      end
+      Facter::Core::Execution.exec("#{ipa} env --server host")
 
-      # Use IPA API to retrieve domain from the client environment
-      domain = Facter::Core::Execution.exec("#{ipa} env domain")
-      domain = domain.strip.split('domain:')[1] unless domain.nil?
-      domain = nil if (domain and domain.strip.empty?)
+      defaults['connected'] = $?.success?
 
-      if domain.nil? or server.nil?
-        status = 'unknown'
-      else
-        status = 'joined'
+      ipa_response = ipa_response.lines.grep(/\S:\s*.+/).map(&:strip).
+        map{ |x|
+          x.split(/:\s+(.*)/)
+        }.flatten
+
+      ipa_response = Hash[*ipa_response]
+
+      ipa_response.keys.each do |key|
+        # Some patch up work for EL6
+        if key =~ /^<(.+)>$/
+          ipa_response[$1] = ipa_response.delete(key)
+        end
       end
-      domain.strip! unless domain.nil?
-      server.strip! unless server.nil?
-      ipa_fact.merge!({ 'status' => status, 'domain' => domain, 'server' => server })
     end
 
-    ipa_fact
+    defaults.merge(ipa_response)
   end
 end
