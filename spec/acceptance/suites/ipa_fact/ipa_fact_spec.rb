a093b3e9de183ad542a5914caf677af440527128
Nick Miller <nick.miller@onyxpoint.com>
2018-01-26 19:12:09 -0500

    (SIMP-4264) simplib: Refine ipa fact (#133)
    (SIMP-4264) simplib: skip EL6 w/FIPS
    - Use return string for Facter::Core::Execution.exec(), because
      $? for execs can be unreliable in Ruby (can be nil upon success)
      and cannot be mocked in Puppet's rspec environment.
    - Rework ipa_spec.rb.  Since the fact is well tested in acceptance
      test, this test is minimal.
    - Add another client test to ipa_fact acceptance test
    - skip EL6 due to fips problems with IPA
    
    The IPA server doesn't support FIPS mode on EL6, so that test should be
    skipped in that scenario.
    
    SIMP-4264 #comment work done
    SIMP-4272 #close

--- spec/acceptance/suites/ipa_fact/ipa_fact_spec.rb
+++ spec/acceptance/suites/ipa_fact/ipa_fact_spec.rb
@@ -2,6 +2,14 @@ require 'spec_helper_acceptance'
 
 test_name 'ipa fact'
 
+def skip_fips(host)
+  if fips_enabled(host) && host.host_hash[:roles].include?('no_fips')
+    return true
+  else
+    return false
+  end
+end
+
 describe 'ipa fact' do
   let (:manifest) {
     <<-EOS
@@ -10,14 +18,46 @@ describe 'ipa fact' do
     EOS
   }
 
-  servers = hosts_with_role(hosts, 'server')
-  servers.each do |server|
+  admin_password = '@dm1n=P@ssw0r!'
+  ipa_domain = 'test.case'
+  ipa_realm = ipa_domain.upcase
+
+  hosts.each do |host|
+    it 'should be running haveged for entropy' do
+      if skip_fips(host)
+        pending("#{host} does not work in FIPS mode")
+        expect(false).to eq true
+
+        next
+      else
+        # IPA requires entropy, so use haveged service
+        on(host, 'puppet resource package epel-release ensure=present')
+        on(host, 'puppet resource package haveged ensure=present')
+        on(host, 'puppet resource service haveged ensure=running enable=true')
+
+        # Install the IPA client on all hosts
+        on(host, 'puppet resource package ipa-client ensure=present')
+
+        # Admintools for EL6
+        on(host, 'puppet resource package ipa-admintools ensure=present', :accept_all_exit_codes => true)
+
+        # Ensure that the hostname is set to the FQDN
+        on(host, "hostname #{fact_on(host, 'fqdn')}")
+      end
+    end
+  end
+
+  hosts_with_role(hosts, 'server').each do |server|
+    next if skip_fips(server)
+
     context 'when IPA is not installed' do
       it 'ipa fact should be nil' do
         results = apply_manifest_on(server, manifest)
         expect(results.output).to match(/Notice: Type => NilClass Content => null/)
-        results = on(server, 'puppet facts')
-        expect(results.output).to_not match(/"ipa": {/)
+
+        results = JSON.load(on(server, 'puppet facts').output)
+
+        expect(results['values']['ipa']).to be_nil
       end
     end
 
@@ -27,51 +67,130 @@ describe 'ipa fact' do
 
         results = apply_manifest_on(server, manifest)
         expect(results.output).to match(/Notice: Type => NilClass Content => null/)
-        results = on(server, 'puppet facts')
-        expect(results.output).to_not match(/"ipa": {/)
+
+        results = JSON.load(on(server, 'puppet facts').output)
+
+        expect(results['values']['ipa']).to be_nil
       end
     end
 
     context 'when IPA is installed and host has joined IPA domain' do
       let(:ipa_domain) { "#{server.name.downcase}.example.com" }
       it 'ipa fact should contain domain and IPA server' do
-        # IPA requires entropy, so use haveged service
-        on(server, 'puppet resource package epel-release ensure=present')
-        on(server, 'puppet resource package haveged ensure=present')
-        on(server, 'puppet resource service haveged ensure=running enable=true')
-
         # ipa-server-install installs both the IPA server and client.
         # The fact uses the client env.
-        fqdn = on(server,'facter fqdn').output.strip
+        fqdn = fact_on(server, 'fqdn')
+
         cmd = [
           'ipa-server-install',
           # IPA realm and domain do not have to match hostname
-          "--domain #{server.name.downcase}.example.com",
-          "--realm #{server.name.upcase}.EXAMPLE.COM",
+          "--domain #{ipa_domain}",
+          "--realm #{ipa_realm}",
           "--hostname #{fqdn}",
-          '--ds-password d1r3ct0ry=P@ssw0r!',
-          '--admin-password @dm1n=P@ssw0r!',
+          '--ds-password "d1r3ct0ry=P@ssw0r!"',
+          "--admin-password '#{admin_password}'",
           '--unattended'
         ]
         puts "\e[1;34m>>>>> The next step takes a very long time ... Please be patient! \e[0m"
         on(server, cmd.join(' '))
         on(server, 'ipactl status')
 
-        results = apply_manifest_on(server, manifest)
-        expect(results.output).to match(/Notice: Type => Hash Content => {"default_domain":"#{ipa_domain}","default_server":"#{fqdn}","status":"joined","domain":"#{ipa_domain}","server":"#{fqdn}"}/)
-        results = on(server, 'puppet facts')
-        expect(results.output).to match(/"ipa": {/)
+        # We only care about this data
+        expect(apply_manifest_on(server, manifest).output).to match(/Hash Content => {"/)
+
+        results = JSON.load(on(server, 'puppet facts').output)
+
+        expect(results['values']['ipa']).to_not be_nil
+        expect(results['values']['ipa']['connected']).to eq true
+        expect(results['values']['ipa']['server']).to eq fqdn
+        expect(results['values']['ipa']['domain']).to eq ipa_domain
+        expect(results['values']['ipa']['realm']).to eq ipa_realm
       end
 
       it 'ipa fact should have unknown status when connection to IPA server is down' do
         # stop IPA server
         on(server, 'ipactl stop')
 
-        fqdn = on(server,'facter fqdn').output.strip
-        results = apply_manifest_on(server, manifest)
-        expect(results.output).to match(/Notice: Type => Hash Content => {"default_domain":"#{ipa_domain}","default_server":"#{fqdn}","status":"unknown","domain":"","server":""}/)
-        results = on(server, 'puppet facts')
-        expect(results.output).to match(/"ipa": {/)
+        results = JSON.load(on(server, 'puppet facts').output)
+
+        expect(results['values']['ipa']).to_not be_nil
+        expect(results['values']['ipa']['connected']).to eq false
+      end
+
+      it 'should restart the IPA server for further tests' do
+        on(server, 'ipactl start')
+      end
+    end
+  end
+
+  hosts_with_role(hosts, 'client').each do |client|
+    next if skip_fips(client)
+
+    context 'as an IPA client' do
+
+      context 'prior to registration' do
+        it 'should not have an IPA fact' do
+          results = JSON.load(on(client, 'puppet facts').output)
+
+          expect(results['values']['ipa']).to be_nil
+        end
+      end
+
+      context 'after registration' do
+        let(:ipa_server) {
+          fact_on(hosts_with_role(hosts, 'server').first, 'fqdn')
+        }
+
+        it 'should register with the IPA server' do
+          ipa_command = [
+            # Unattended installation
+            'ipa-client-install -U',
+            # IPA directory domain
+            "--domain=#{ipa_domain}",
+            # IPA server to use
+            "--server=#{ipa_server}",
+            # Only point at this server and don't use SRV
+            '--fixed-primary',
+            # IPA krb5 realm
+            "--realm=#{ipa_realm}",
+            # Krb5 principal name to use
+            '--principal=admin',
+            # Admin password
+            "--password='#{admin_password}'",
+            # Don't update using authconfig
+            '--noac'
+          ].join(' ')
+
+          on(client, ipa_command)
+        end
+
+        it 'should have the IPA fact populated' do
+          results = JSON.load(on(client, 'puppet facts').output)
+
+          expect(results['values']['ipa']).to_not be_nil
+          expect(results['values']['ipa']['connected']).to eq true
+          expect(results['values']['ipa']['server']).to eq ipa_server
+          expect(results['values']['ipa']['domain']).to eq ipa_domain
+          expect(results['values']['ipa']['realm']).to eq ipa_realm
+        end
+
+        it 'ipa fact should have unknown status when connection to IPA server is down' do
+          # stop IPA server
+          hosts_with_role(hosts, 'server').each do |server|
+            on(server, 'ipactl stop')
+          end
+
+          results = JSON.load(on(client, 'puppet facts').output)
+
+          expect(results['values']['ipa']).to_not be_nil
+          expect(results['values']['ipa']['connected']).to eq false
+        end
+
+        it 'should restart the IPA server for further tests' do
+          hosts_with_role(hosts, 'server').each do |server|
+            on(server, 'ipactl start')
+          end
+        end
       end
     end
   end
--- spec/unit/facter/ipa_spec.rb
+++ spec/unit/facter/ipa_spec.rb
@@ -1,34 +1,45 @@
 require 'spec_helper'
 
 describe "custom fact ipa" do
-  let (:default_conf) {
-    [
-      "host = client.example.com\n",
-      "basedn = dc=example,dc=com\n",
-      "realm = EXAMPLE.COM\n",
-      # In next 2 config lines, artificially prepend domain and IPA
-      # server with 'default.' so we can validate parsing.
-      "domain = default.example.com\n",
-      "xmlrpc_uri = https://default.ipaserver.example.com/ipa/xml\n",
-      "ldap_uri = ldapi://%2fvar%2frun%2fslapd-EXAMPLE-COM.socket\n",
-      "enable_ra = True\n",
-      "ra_plugin = dogtag\n",
-      "dogtag_version = 10\n",
-      "mode = production\n"
-    ]
+  let (:ipa_env_query) {
+    '/usr/bin/ipa env domain server realm basedn tls_ca_cert'
   }
 
-  let (:host_info) { <<EOM
-  Host name: client.example.com
-  Principal name: host/client.example.com@EXAMPLE.COM
-  Principal alias: host/client.example.com@EXAMPLE.COM
-  SSH public key fingerprint: SHA256:m8TatOcxcXkhtd80EQjJUJG2zYUctl1EkoroadusTeU (ssh-rsa),
-                              SHA256:XI7tRHoZuQJ7nc63t0hlVaQ0sP/RJcvMmMAt83jwVzE (ecdsa-sha2-nistp256),
-                              SHA256:PmgNsbCMj40etrpL+f5lLnDGLWpwkvb/s7RmYTKQKfE (ssh-ed25519)
-  Password: False
-  Keytab: True
-  Managed by: ipaserver.example.com
-EOM
+  let (:ipa_env_server_query) {
+    '/usr/bin/ipa env --server host'
+  }
+
+  let (:default_conf) {
+    [
+      '#host = client.example.com',
+      'host=client.example.com',
+      'basedn = dc=example,dc=com',
+      'realm = EXAMPLE.COM',
+      'domain = example.com',
+      'server = ipaserver.example.com',
+      'xmlrpc_uri = https://ipaserver.example.com/ipa/xml',
+      'ldap_uri = ldapi://%2fvar%2frun%2fslapd-EXAMPLE-COM.socket',
+      'enable_ra = True',
+      'ra_plugin = dogtag',
+      'dogtag_version = 10',
+      'mode = production'
+    ].join("\n")
+  }
+
+  let (:ipa_env) {
+    [
+      '  domain: example.com',
+      '  server: ipaserver.example.com',
+      '  realm:  EXAMPLE.COM',
+      '  basedn: dc=example,dc=com',
+      '-----------',
+      '4 variables',
+      '-----------'
+    ].join("\n")
+  }
+
+  let (:ipa_server_env) {
+    'host: ipaserver.example.com'
   }
 
   before(:each) do
@@ -38,203 +49,62 @@ EOM
     Facter::Core::Execution.stubs(:exec).with('uname -s').returns('Linux')
   end
 
-  context 'host is joined to IPA domain and can communicate with IPA server' do
-    it 'should return hash with joined status, IPA domain and IPA server' do
-      Facter::Core::Execution.expects(:which).with('kinit').returns('/usr/bin/kinit')
-      Facter::Core::Execution.expects(:which).with('ipa').returns('/usr/bin/ipa')
-      File.expects(:exist?).with('/etc/ipa/default.conf').returns(true)
-      IO.expects(:readlines).with('/etc/ipa/default.conf').returns(default_conf)
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/kinit -k 2>&1').returns('')
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/ipa env host').returns("  host: client.example.com\n")
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/ipa host-show client.example.com').returns(host_info)
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/ipa env domain').returns("  domain: example.com\n")
+  context 'host is joined to IPA domain' do
+    context 'IPA server is available' do
+      context 'kinit is not required' do
+        it 'should execute only ipa commands and report local env + connected status' do
+          Facter::Core::Execution.expects(:which).with('kinit').returns('/usr/bin/kinit')
+          Facter::Core::Execution.expects(:which).with('ipa').returns('/usr/bin/ipa')
+          File.expects(:exist?).with('/etc/ipa/default.conf').returns(true)
+          File.expects(:read).with('/etc/ipa/default.conf').returns(default_conf)
+          Facter::Core::Execution.expects(:exec).with(ipa_env_query).returns(ipa_env)
+          Facter::Core::Execution.expects(:exec).with(ipa_env_server_query).returns(ipa_server_env)
+          expect(Facter.fact('ipa').value).to eq({
+            'connected' => true,
+            'domain'    => 'example.com',
+            'server'    => 'ipaserver.example.com',
+            'realm'     => 'EXAMPLE.COM',
+            'basedn'    => 'dc=example,dc=com'
+          })
+        end
+      end
 
-      expect(Facter.fact('ipa').value).to eq({
-        'default_domain' => 'default.example.com',
-        'default_server' => 'default.ipaserver.example.com',
-        'status' => 'joined',
-        'domain' => 'example.com',
-        'server' => 'ipaserver.example.com'
-      })
-    end
-  end
-
-  context 'kinit executable is not available' do
-    it 'should return nil' do
-      Facter::Core::Execution.expects(:which).with('kinit').returns(nil)
-      Facter::Core::Execution.expects(:which).with('ipa').returns('/usr/bin/ipa')
-
-      expect(Facter.fact('ipa').value).to be nil
-    end
-  end
-
-  context 'ipa executable is not available' do
-    it 'should return nil' do
-      Facter::Core::Execution.expects(:which).with('kinit').returns('/usr/bin/kinit')
-      Facter::Core::Execution.expects(:which).with('ipa').returns(nil)
-
-      expect(Facter.fact('ipa').value).to be nil
-    end
-  end
-
-  context '/etc/ipa/default.conf is not available' do
-    it 'should return nil' do
-      Facter::Core::Execution.expects(:which).with('kinit').returns('/usr/bin/kinit')
-      Facter::Core::Execution.expects(:which).with('ipa').returns('/usr/bin/ipa')
-      File.expects(:exist?).with('/etc/ipa/default.conf').returns(false)
-
-      expect(Facter.fact('ipa').value).to be nil
-    end
-  end
-
-  context 'kinit fails' do
-    it "should return hash of unknown status and nil IPA domain and server if 'kinit' returns nil" do
-      Facter::Core::Execution.expects(:which).with('kinit').returns('/usr/bin/kinit')
-      Facter::Core::Execution.expects(:which).with('ipa').returns('/usr/bin/ipa')
-      File.expects(:exist?).with('/etc/ipa/default.conf').returns(true)
-      IO.expects(:readlines).with('/etc/ipa/default.conf').returns(default_conf)
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/kinit -k 2>&1').returns(nil)
-
-      expect(Facter.fact('ipa').value).to eq({
-        'default_domain' => 'default.example.com',
-        'default_server' => 'default.ipaserver.example.com',
-        'status' => 'unknown',
-        'domain' => nil,
-        'server' => nil
-      })
+      context 'kinit is required' do
+        it 'should execute kinit + ipa commands and return local env + connected status' do
+          Facter::Core::Execution.expects(:which).with('kinit').returns('/usr/bin/kinit')
+          Facter::Core::Execution.expects(:which).with('ipa').returns('/usr/bin/ipa')
+          File.expects(:exist?).with('/etc/ipa/default.conf').returns(true)
+          File.expects(:read).with('/etc/ipa/default.conf').returns(default_conf)
+          Facter::Core::Execution.expects(:exec).with('/usr/bin/kinit -k 2>&1').returns('')
+          Facter::Core::Execution.expects(:exec).twice.with( ipa_env_query).returns('', ipa_env)
+          Facter::Core::Execution.expects(:exec).with(ipa_env_server_query).returns(ipa_server_env)
+          expect(Facter.fact('ipa').value).to eq({
+            'connected' => true,
+            'domain'    => 'example.com',
+            'server'    => 'ipaserver.example.com',
+            'realm'     => 'EXAMPLE.COM',
+            'basedn'    => 'dc=example,dc=com'
+          })
+        end
+      end
     end
 
-    it "should return hash with unknown status and nil IPA domain and server if 'kinit' fails" do
-      Facter::Core::Execution.expects(:which).with('kinit').returns('/usr/bin/kinit')
-      Facter::Core::Execution.expects(:which).with('ipa').returns('/usr/bin/ipa')
-      File.expects(:exist?).with('/etc/ipa/default.conf').returns(true)
-      IO.expects(:readlines).with('/etc/ipa/default.conf').returns(default_conf)
-      err_msg = "kinit: Cannot contact any KDC for realm 'EXAMPLE.COM' while getting initial credentials"
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/kinit -k 2>&1').returns(err_msg)
-
-      expect(Facter.fact('ipa').value).to eq({
-        'default_domain' => 'default.example.com',
-        'default_server' => 'default.ipaserver.example.com',
-        'status' => 'unknown',
-        'domain' => nil,
-        'server' => nil
-      })
-    end
-  end
-
-  context 'ipa server is not available from the IPA client environment' do
-    it "should return hash with unknown status and nil server if exec of 'ipa env host' returns nil" do
-      Facter::Core::Execution.expects(:which).with('kinit').returns('/usr/bin/kinit')
-      Facter::Core::Execution.expects(:which).with('ipa').returns('/usr/bin/ipa')
-      File.expects(:exist?).with('/etc/ipa/default.conf').returns(true)
-      IO.expects(:readlines).with('/etc/ipa/default.conf').returns(default_conf)
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/kinit -k 2>&1').returns('')
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/ipa env host').returns(nil)
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/ipa env domain').returns("  domain: example.com")
-
-      expect(Facter.fact('ipa').value).to eq({
-        'default_domain' => 'default.example.com',
-        'default_server' => 'default.ipaserver.example.com',
-        'status' => 'unknown',
-        'domain' => 'example.com',
-        'server' => nil
-      })
-    end
-
-    it "should return hash with unknown status and nil server if exec of 'ipa env host' is empty" do
-      Facter::Core::Execution.expects(:which).with('kinit').returns('/usr/bin/kinit')
-      Facter::Core::Execution.expects(:which).with('ipa').returns('/usr/bin/ipa')
-      File.expects(:exist?).with('/etc/ipa/default.conf').returns(true)
-      IO.expects(:readlines).with('/etc/ipa/default.conf').returns(default_conf)
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/kinit -k 2>&1').returns('')
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/ipa env host').returns("\n")
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/ipa env domain').returns("  domain: example.com")
-
-      expect(Facter.fact('ipa').value).to eq({
-        'default_domain' => 'default.example.com',
-        'default_server' => 'default.ipaserver.example.com',
-        'status' => 'unknown',
-        'domain' => 'example.com',
-        'server' => nil
-      })
-    end
-
-    it "should return hash with unknown status and nil server if 'ipa host-show' fails" do
-      Facter::Core::Execution.expects(:which).with('kinit').returns('/usr/bin/kinit')
-      Facter::Core::Execution.expects(:which).with('ipa').returns('/usr/bin/ipa')
-      File.expects(:exist?).with('/etc/ipa/default.conf').returns(true)
-      IO.expects(:readlines).with('/etc/ipa/default.conf').returns(default_conf)
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/kinit -k 2>&1').returns('')
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/ipa env host').returns("  host: client.example.com\n")
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/ipa host-show client.example.com').returns(nil)
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/ipa env domain').returns("  domain: example.com")
-
-      expect(Facter.fact('ipa').value).to eq({
-        'default_domain' => 'default.example.com',
-        'default_server' => 'default.ipaserver.example.com',
-        'status' => 'unknown',
-        'domain' => 'example.com',
-        'server' => nil
-      })
-    end
-
-    it "should return hash with unknown status and nil server if 'ipa host-show' does not have 'Managed by:' line" do
-      Facter::Core::Execution.expects(:which).with('kinit').returns('/usr/bin/kinit')
-      Facter::Core::Execution.expects(:which).with('ipa').returns('/usr/bin/ipa')
-      File.expects(:exist?).with('/etc/ipa/default.conf').returns(true)
-      IO.expects(:readlines).with('/etc/ipa/default.conf').returns(default_conf)
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/kinit -k 2>&1').returns('')
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/ipa env host').returns("  host: client.example.com\n")
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/ipa host-show client.example.com').returns('')
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/ipa env domain').returns("  domain: example.com")
-
-      expect(Facter.fact('ipa').value).to eq({
-        'default_domain' => 'default.example.com',
-        'default_server' => 'default.ipaserver.example.com',
-        'status' => 'unknown',
-        'domain' => 'example.com',
-        'server' => nil
-      })
-    end
-  end
-
-  context 'when ipa domain is not available from the IPA client environment' do
-    it 'should return hash with unknown status and nil domain if domain is nil' do
-      Facter::Core::Execution.expects(:which).with('kinit').returns('/usr/bin/kinit')
-      Facter::Core::Execution.expects(:which).with('ipa').returns('/usr/bin/ipa')
-      File.expects(:exist?).with('/etc/ipa/default.conf').returns(true)
-      IO.expects(:readlines).with('/etc/ipa/default.conf').returns(default_conf)
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/kinit -k 2>&1').returns('')
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/ipa env host').returns("  host: client.example.com\n")
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/ipa host-show client.example.com').returns(host_info)
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/ipa env domain').returns(nil)
-
-      expect(Facter.fact('ipa').value).to eq({
-        'default_domain' => 'default.example.com',
-        'default_server' => 'default.ipaserver.example.com',
-        'status' => 'unknown',
-        'domain' => nil,
-        'server' => 'ipaserver.example.com'
-      })
-    end
-
-    it 'should return hash with unknown status and nil domain if domain is empty' do
-      Facter::Core::Execution.expects(:which).with('kinit').returns('/usr/bin/kinit')
-      Facter::Core::Execution.expects(:which).with('ipa').returns('/usr/bin/ipa')
-      File.expects(:exist?).with('/etc/ipa/default.conf').returns(true)
-      IO.expects(:readlines).with('/etc/ipa/default.conf').returns(default_conf)
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/kinit -k 2>&1').returns('')
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/ipa env host').returns("  host: client.example.com\n")
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/ipa host-show client.example.com').returns(host_info)
-      Facter::Core::Execution.expects(:exec).with('/usr/bin/ipa env domain').returns("\n")
-
-      expect(Facter.fact('ipa').value).to eq({
-        'default_domain' => 'default.example.com',
-        'default_server' => 'default.ipaserver.example.com',
-        'status' => 'unknown',
-        'domain' => nil,
-        'server' => 'ipaserver.example.com'
-      })
+    context 'IPA server is not available' do
+      it 'should return defaults from /etc/ipa/default.conf and disconnected status' do
+        Facter::Core::Execution.expects(:which).with('kinit').returns('/usr/bin/kinit')
+        Facter::Core::Execution.expects(:which).with('ipa').returns('/usr/bin/ipa')
+        File.expects(:exist?).with('/etc/ipa/default.conf').returns(true)
+        File.expects(:read).with('/etc/ipa/default.conf').returns(default_conf)
+        Facter::Core::Execution.expects(:exec).with('/usr/bin/kinit -k 2>&1').returns('some error message')
+        Facter::Core::Execution.expects(:exec).twice.with(ipa_env_query).returns('')
+        expect(Facter.fact('ipa').value).to eq({
+          'connected' => false,
+          'domain'    => 'example.com',
+          'server'    => 'ipaserver.example.com',
+          'realm'     => 'EXAMPLE.COM',
+          'basedn'    => 'dc=example,dc=com'
+        })
+      end
     end
   end
 end
