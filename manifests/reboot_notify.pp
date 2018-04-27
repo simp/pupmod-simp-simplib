e58765697813a49ee273bdd9d4ddcba7e6afcc47
jeannegreulich <jeanne.greulich@onyxpoint.com>
2018-04-27 16:56:36 -0400

    Update reboot_notify.pp

--- manifests/reboot_notify.pp
+++ manifests/reboot_notify.pp
@@ -3,9 +3,13 @@
 #
 # @param log_level
 #   The Puppet log_level to use when generating output
+#   
+#   To change the level of the reboot_notify messages add this class 
+#   to the class list in hiera and set simplib::reboot_notify::log_level to
+#   the level you want.
 #
-#   * Set to ``debug`` if you wish to disable output unless you're running in
-#     debug mode.
+#   * Set to  log_level to``debug`` if you wish to disable output unless you're
+#     running in debug mode.
 #
 class simplib::reboot_notify (
   Simplib::PuppetLogLevel $log_level = 'notice'
