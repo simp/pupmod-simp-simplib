aab1894af6db9ec02136fb3f9212008d686ed87f
Nick Miller <nick.miller@onyxpoint.com>
2018-05-08 11:29:24 -0400

    Add tweaks for generating options strings

--- functions/hash_to_opts.pp
+++ functions/hash_to_opts.pp
@@ -1,25 +1,42 @@
-# Turn a hash into a string with eash key prefixed
-# with '--' and connected to each value with '='
+# Turn a hash into a options string, for use in a shell command
 #
 # @example simplib::hash_to_opts({'key' => 'value'})
-#   returns `--key=value`
+#   returns ``--key=value``
 # @example simplib::hash_to_opts({'key' => ['lo',7,false]})
-#   returns `--key="lo,7,false"`
+#   returns ``--key=lo,7,false``
 # @example simplib::hash_to_opts({'key' => Undef })
-#   returns `--key`
+#   returns ```--key``
+# @example simplib::hash_to_opts({'f' => '/tmp/file'}, {'connector' => ' ', 'prefix' => '-'})
+#   returns ``-f /tmp/file``
 #
 # @param input Input hash, with Strings as keys and either a String, Array,
 #   Numeric, Boolean, or Undef as a value.
 #
+# @param opts Options hash. It only takes 3 keys, none of them required:
+#   * ``connector``: String that joins each key and value pair. Defaults to '='
+#   * ``prefix``: String that prefixes each key value pair. Defaults to '--'
+#   * ``array_delimiter``: When a value is an array, the string that is used to
+#     deliminate each item. Defaults to ','
+#
 # @return [String]
 #
-function simplib::hash_to_opts(Hash[String,Variant[Array,String,Numeric,Boolean,Undef]] $input) {
+function simplib::hash_to_opts (
+  Hash[String,Variant[Array,String,Numeric,Boolean,Undef]] $input,
+  Struct[{
+    Optional[connector]       => String[1],
+    Optional[prefix]          => String[1],
+    Optional[array_delimiter] => String[1],
+  }] $opts = {}
+) {
+  $connector       = pick($opts['connector'],       '=')
+  $prefix          = pick($opts['prefix'],          '--')
+  $array_delimiter = pick($opts['array_delimiter'], ',')
+
   $out = $input.map |$key, $val| {
     case $val {
-      Undef:         { "--${key}"                      }
-      Array:         { "--${key}=\"${val.join(',')}\"" }
-      /[[:blank:]]/: { "--${key}=\"${val}\""           }
-      default:       { "--${key}=${val}"               }
+      Undef:   { "${prefix}${key}" }
+      Array:   { "${prefix}${key}${connector}${val.join($array_delimiter).shellquote}" }
+      default: { "${prefix}${key}${connector}${String($val).shellquote}" }
     }
   }
   $out.join(' ')
--- spec/functions/hash_to_opts_spec.rb
+++ spec/functions/hash_to_opts_spec.rb
@@ -2,31 +2,72 @@ require 'spec_helper'
 
 describe 'simplib::hash_to_opts' do
 
+  tests = [
+    {
+      content: {'iface' => 'eth1'},
+      result: '--iface=eth1'
+    },
+    {
+      content: { 'iface' => 'eth1', 'arg2'  => 'junk value' },
+      result: '--iface=eth1 --arg2="junk value"'
+    },
+    {
+      content: {'key' => 8},
+      result: '--key=8'
+    },
+    {
+      content: {'key' => false},
+      result: '--key=false'
+    },
+    {
+      content: {'key' => ['yes',true,1]},
+      result: '--key=yes,true,1'
+    },
+    {
+      content: {'key' => :undef},
+      result: '--key'
+    },
+    {
+      content: {'key' => nil},
+      result: '--key'
+    },
+  ]
+
+
   it { is_expected.to run.with_params() \
-    .and_raise_error(/expects 1 argument, got none/) }
+    .and_raise_error(/between 1 and 2 arguments, got none/) }
 
-  # basic hash
-  it { is_expected.to run.with_params({'iface' => 'eth1'}).and_return('--iface=eth1') }
+  context 'with default secondary options' do
+    tests.each do |params|
+      it { is_expected.to run.with_params(params[:content]) \
+        .and_return(params[:result]) }
+    end
+  end
 
-  # with value that has a space
-  bigger_hash = {
-    'iface' => 'eth1',
-    'arg2'  => 'junk value'
-  }
-  it { is_expected.to run.with_params(bigger_hash) \
-    .and_return('--iface=eth1 --arg2="junk value"') }
+  context 'with some secondary options set' do
+    tests.each do |params|
+      opts = { 'connector' => ' ' }
+      result = params[:result].gsub(/=/,' ')
 
-  # value that is a number
-  it { is_expected.to run.with_params({'key' => 8}).and_return('--key=8') }
+      it { is_expected.to run.with_params(params[:content],opts) \
+        .and_return(result) }
+    end
+  end
 
-  # value that is a boolean
-  it { is_expected.to run.with_params({'key' => false}).and_return('--key=false') }
+  context 'with all secondary options set' do
+    tests.each do |params|
+      opts = {
+        'connector' => ' ',
+        'prefix'    => '-',
+        'array_delimiter' => ':'
+      }
+      result = params[:result] \
+        .gsub(/=/,' ')
+        .gsub(/--/,'-')
+        .gsub(/,/,':')
 
-  # value that is an array
-  it { is_expected.to run.with_params({'key' => ['yes',true,1]}).and_return('--key="yes,true,1"') }
-
-  # key with no value
-  it { is_expected.to run.with_params({'key' => :undef}).and_return('--key') }
-  it { is_expected.to run.with_params({'key' => nil}).and_return('--key') }
-
+      it { is_expected.to run.with_params(params[:content],opts) \
+        .and_return(result) }
+    end
+  end
 end
