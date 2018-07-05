5260e0bb4761df99b8f92a88eb8cbf2328140341
Trevor Vaughan <tvaughan@onyxpoint.com>
2018-07-05 08:49:16 -0400

    Added check for author to "module_exist"

--- lib/puppet/functions/simplib/module_exist.rb
+++ lib/puppet/functions/simplib/module_exist.rb
@@ -1,5 +1,7 @@
 # Determines if a module exists in the current environment
 #
+# If passed with an author, such as `simp/simplib` or `simp-simplib`, will
+# return whether or not that *specific* module exists.
 Puppet::Functions.create_function(:'simplib::module_exist') do
 
   # @param module_name The module name to check
@@ -9,8 +11,23 @@ Puppet::Functions.create_function(:'simplib::module_exist') do
   end
 
   def module_exist(module_name)
-    if Puppet::Module.find(module_name, closure_scope.compiler.environment.to_s)
-      return true
+    _module_author, _module_name = module_name.split(%r(/|-))
+
+    unless _module_name
+      _module_name = _module_author.dup
+      _module_author = nil
+    end
+
+    if Puppet::Module.find(_module_name, closure_scope.compiler.environment.to_s)
+      if _module_author
+        if _module_author.strip == call_function('load_module_metadata', _module_name)['name'].strip.split(%r(/|-)).first
+          return true
+        else
+          return false
+        end
+      else
+        return true
+      end
     else
       return false
     end
