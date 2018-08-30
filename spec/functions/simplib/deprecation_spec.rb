3235eca1c6773e1d660af8e807152d0e4d74f840
Trevor Vaughan <tvaughan@onyxpoint.com>
2018-08-30 18:03:58 -0400

    properly unstub the deprecation warnings

--- spec/functions/simplib/deprecation_spec.rb
+++ spec/functions/simplib/deprecation_spec.rb
@@ -4,15 +4,17 @@ describe 'simplib::deprecation' do
   context 'with SIMPLIB_LOG_DEPRECATIONS environment variable = "true" ' do
     it 'should display a single warning' do
       ENV['SIMPLIB_LOG_DEPRECATIONS'] = 'true'
-      Puppet.expects(:warning).at_least_once
       Puppet.expects(:warning).with(includes('test_func is deprecated'))
+      Puppet.stubs(:warning).with(Not(includes('test_func is deprecated')))
+
       is_expected.to run.with_params('test_key', 'test_func is deprecated')
     end
 
     it 'should display a single warning, despite multiple calls' do
       ENV['SIMPLIB_LOG_DEPRECATIONS'] = 'true'
-      Puppet.expects(:warning).at_least_once
       Puppet.expects(:warning).with(includes('test_func is deprecated')).once
+      Puppet.stubs(:warning).with(Not(includes('test_func is deprecated')))
+
       is_expected.to run.with_params('test_key', 'test_func is deprecated')
       is_expected.to run.with_params('test_key', 'test_func is deprecated')
     end
@@ -22,8 +24,9 @@ describe 'simplib::deprecation' do
     it 'should not display a warning' do
       ENV['SIMPLIB_LOG_DEPRECATIONS'] = nil
       # This is working around deprecation warnings that get added by Puppet core
-      Puppet.expects(:warning).at_most(99)
       Puppet.expects(:warning).with(includes('test_func is deprecated')).never
+      Puppet.stubs(:warning).with(Not(includes('test_func is deprecated')))
+
       is_expected.to run.with_params('test_key', 'test_func is deprecated')
     end
   end
