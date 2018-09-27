ed1d332bf9b1fcf60be6bd51f113d1e8d8ce4fb3
Daniel Henninger <daniel@ncsu.edu>
2018-09-27 09:57:49 -0400

    Fix issue with uid_min where /etc/login.defs is missing and general logic with unless.

--- lib/facter/uid_min.rb
+++ lib/facter/uid_min.rb
@@ -4,16 +4,15 @@
 #
 Facter.add('uid_min') do
   setcode do
+    uid_min = ''
     if File.readable?('/etc/login.defs')
       uid_min = File.open('/etc/login.defs').grep(/UID_MIN/)
       unless uid_min.empty?
         uid_min = uid_min.first.to_s.chomp.split.last
-      else
-        uid_min = ''
       end
     end
 
-    unless uid_min || uid_min.empty?
+    unless uid_min.empty?
       if ['RedHat','CentOS','OracleLinux','Scientific'].include?(Facter.value(:operatingsystem)) &&
          Facter.value(:operatingsystemmajrelease) < '7' then
           uid_min = '500'
