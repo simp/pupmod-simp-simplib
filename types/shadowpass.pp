5262478b890a42ec7960043b9ab89ec9a9df472e
Trevor Vaughan <tvaughan@onyxpoint.com>
2019-01-04 16:24:35 -0500

    Refactoring
    * Simplified data type
    * Updated test for all known valid crypt entries (systems may vary)
    * Updated travis.yml to attempt and fix bundler version issue

--- types/shadowpass.pp
+++ types/shadowpass.pp
@@ -1,2 +1,5 @@
 # /etc/shadow password allowed values
-type Simplib::ShadowPass = Pattern['(^\*$|^!{1,2}.?|^\$([156]|2[ay]){1}\$.+)']
+type Simplib::ShadowPass = Variant[
+  Enum['*','!','!!'],
+  Pattern['^((!.*)|(\$(1|2(a|y)?|3|md5|sha1|5|6)\$.+))$']
+]
