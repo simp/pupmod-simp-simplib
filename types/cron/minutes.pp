156738e61d8c233a7d8823785872f6bcb59f29f3
sharkbruhaha <shark.bruhaha@gmail.com>
2019-03-04 20:14:39 -0500

    Working on spec tests

--- types/cron/minutes.pp
+++
@@ -1,3 +0,0 @@
-# Matches valid cron minute parameter
-#
-type Simplib::Cron::Minute = Pattern['\\*|(?:\\*)?(?:\/(?:[0-5]?\\d))|(?:[0-5]?\\d)(?:(?:-|\/|,)(?:[0-5]?\\d))?(?:,(?:[0-5]?\\d)(?:(?:-|\/|,)(?:[0-5]?\\d))?)*']
