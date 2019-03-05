62c8988272c168a1c068d35d4769f55daa03f3b3
sharkbruhaha <shark.bruhaha@gmail.com>
2019-03-05 16:44:45 -0500

    Fixed regex, added spec tests

--- types/cron/weekday.pp
+++ types/cron/weekday.pp
@@ -1,3 +1,3 @@
 # Matches valid cron weekday parameter
 #
-type Simplib::Cron::WeekDay = Pattern['\\?|\\*|(?:\\*)?(?:\/(?:[0-7]))|L|(?:[0-7])(?:(?:#[1-5])|(?:(?:-|\/|,)[0-7](?:L)?))?(?:(?:,[0-7])(?:(?:#[1-5])|(?:(?:-|\/|,)[0-7](?:L)?)))*|(?:SUN|MON|TUE|WED|THU|FRI|SAT|sun|mon|tue|wed|thu|fri|sat)(?:#[1-5])|(?:SUN|MON|TUE|WED|THU|FRI|SAT|sun|mon|tue|wed|thu|fri|sat)(?:,(?:SUN|MON|TUE|WED|THU|FRI|SAT|sun|mon|tue|wed|thu|fri|sat))*']
+type Simplib::Cron::WeekDay = Pattern['^(?:\?|\*|(?:\*)?(?:\/(?:[0-7]))|L|(?:[0-7])(?:(?:#[1-5])|(?:(?:(?:-|\/|,)[0-7])?(?:L)?))?(?:(?:,[0-7])(?:(?:#[1-5])|(?:(?:(?:-|\/|,)[0-7])?(?:L)?)))*|(?:SUN|MON|TUE|WED|THU|FRI|SAT|sun|mon|tue|wed|thu|fri|sat)(?:#[1-5])|(?:SUN|MON|TUE|WED|THU|FRI|SAT|sun|mon|tue|wed|thu|fri|sat)(?:,(?:SUN|MON|TUE|WED|THU|FRI|SAT|sun|mon|tue|wed|thu|fri|sat))*)$']
