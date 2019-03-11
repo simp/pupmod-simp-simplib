a0c2a3d60a71668a58a2dce2ab3cf54ac1ac241c
sharkbruhaha <shark.bruhaha@gmail.com>
2019-03-11 12:19:50 -0400

    Moved regex words to Enum[] for\nquicker parsing.

--- types/cron/month.pp
+++ types/cron/month.pp
@@ -3,6 +3,6 @@
 # Tested with Rubular: https://rubular.com/r/TSDNxt1rcWkb8U
 type Simplib::Cron::Month = Variant[Integer[1,12],Pattern['^(?x)(?:\*|
 (?:\*\/(?:[1-9]|1[012]))|
-(?:(?:[1-9]|1[012])(?:(?:-(?:[1-9]|1[012]))(?:\/(?:[1-9]|1[012]))?)?)|
-(?i)(?:JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)
-)$']]
+(?:(?:[1-9]|1[012])(?:(?:-(?:[1-9]|1[012]))(?:\/(?:[1-9]|1[012]))?)?)
+)$'],
+Enum['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']]
--- types/cron/weekday.pp
+++ types/cron/weekday.pp
@@ -3,6 +3,6 @@
 # Tested with Rubular: https://rubular.com/r/uuFFu5ISzdRL7l
 type Simplib::Cron::WeekDay = Variant[Integer[1,7],Pattern['^(?x)(?:\*|
 (?:\*\/(?:[0-7]))|
-(?:(?:[0-7])(?:(?:-[0-7])(?:\/[0-7])?)?)|
-(?i)(?:SUN|MON|TUE|WED|THU|FRI|SAT)
-)$']]
+(?:(?:[0-7])(?:(?:-[0-7])(?:\/[0-7])?)?)
+)$'],
+Enum['SUN','MON','TUE','WED','THU','FRI','SAT']]
