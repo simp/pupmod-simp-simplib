4332b76cc5dc0c403a1a6b073aaf6079c6e7984f
Trevor Vaughan <tvaughan@onyxpoint.com>
2019-03-25 18:30:00 -0400

    Update lib/puppet/parser/functions/bracketize.rb
    Co-Authored-By: sharkbruhaha <44031572+sharkbruhaha@users.noreply.github.com>

--- lib/puppet/parser/functions/bracketize.rb
+++ lib/puppet/parser/functions/bracketize.rb
@@ -8,7 +8,7 @@ module Puppet::Parser::Functions
     @return [Variant[String, Array[String]]]
     ENDHEREDOC
 
-function_simplib_deprecation(['bracketize', 'bracketize is deprecated, please use tcpwrappers::bracketize'])
+function_simplib_deprecation(['bracketize', 'bracketize is deprecated'])
 
     ipaddr = Array(ipaddr).flatten
     toret = ipaddr.map { |x|
