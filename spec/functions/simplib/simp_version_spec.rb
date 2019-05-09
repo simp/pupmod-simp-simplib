96d2fec41bb9ba1d20d93950934ed92eb37833e5
jeannegreulich <jeanne.greulich@onyxpoint.com>
2019-05-09 13:58:32 -0400

    (SIMP-6506) Out Of Memory Error
    - updated simp_version function to use Puppet::Util::Execution.execute
      instead of backtics.  This avoids a GLIBC error triggered by JRuby 9K.
    
    SIMP-6506 #comment Also needs simp module update.

--- spec/functions/simplib/simp_version_spec.rb
+++ spec/functions/simplib/simp_version_spec.rb
@@ -5,12 +5,14 @@ describe 'simplib::simp_version' do
   context 'a valid version exists in /etc/simp/simp.version' do
     it 'should return the version with whitespace retained' do
       File.expects(:read).with('/etc/simp/simp.version').returns(" 6.4.0-0\n")
+      File.stubs(:readable?).with('/etc/simp/simp.version').returns(true)
       File.stubs(:read).with(regexp_matches(/metadata.json/), {:encoding => "utf-8"}).returns('')
 
       is_expected.to run.and_return(" 6.4.0-0\n")
     end
 
     it "should return the version with 'simp-' stripped" do
+      File.stubs(:readable?).with('/etc/simp/simp.version').returns(true)
       File.stubs(:read).with('/etc/simp/simp.version').returns("simp-5.4.0-0\n")
       File.stubs(:read).with(regexp_matches(/metadata.json/), {:encoding => "utf-8"}).returns('')
 
@@ -18,6 +20,7 @@ describe 'simplib::simp_version' do
     end
 
     it 'should return the version with whitespace stripped when stripping is enabled' do
+      File.stubs(:readable?).with('/etc/simp/simp.version').returns(true)
       File.stubs(:read).with('/etc/simp/simp.version').returns("6.4.0-0\n")
       File.stubs(:read).with(regexp_matches(/metadata.json/), {:encoding => "utf-8"}).returns('')
 
@@ -28,6 +31,7 @@ describe 'simplib::simp_version' do
   context '/etc/simp/simp.version is empty' do
     it 'should return unknown' do
       File.stubs(:read).with('/etc/simp/simp.version').returns("")
+      File.stubs(:readable?).with('/etc/simp/simp.version').returns(true)
       File.stubs(:read).with(regexp_matches(/metadata.json/), {:encoding => "utf-8"}).returns('')
 
       is_expected.to run.and_return("unknown\n")
@@ -41,19 +45,15 @@ describe 'simplib::simp_version' do
 
     context 'rpm query succeeds' do
       it 'should return the version with whitespace retained' do
-        File.stubs(:read).with('/etc/simp/simp.version').raises(Errno::ENOENT)
-        File.stubs(:read).with(regexp_matches(/metadata.json/), {:encoding => "utf-8"}).returns('')
-        subject.func.stubs(:`).with(rpm_query).returns("6.4.0-0\n")
-        $?.expects(:success?).returns(true)
+        File.stubs(:readable?).with('/etc/simp/simp.version').returns(false)
+        Puppet::Util::Execution.stubs(:execute).with(rpm_query, {:failonfail => true}).returns("6.4.0-0\n")
 
         is_expected.to run.and_return("6.4.0-0\n")
       end
 
       it 'should return the version with whitespace stripped when stripping is enabled' do
-        File.stubs(:read).with('/etc/simp/simp.version').raises(Errno::ENOENT)
-        File.stubs(:read).with(regexp_matches(/metadata.json/), {:encoding => "utf-8"}).returns('')
-        subject.func.stubs(:`).with(rpm_query).returns("6.4.0-0\n")
-        $?.expects(:success?).returns(true)
+        File.stubs(:readable?).with('/etc/simp/simp.version').returns(false)
+        Puppet::Util::Execution.stubs(:execute).with(rpm_query, {:failonfail => true}).returns("6.4.0-0\n")
 
         is_expected.to run.with_params(true).and_return('6.4.0-0')
       end
@@ -61,10 +61,12 @@ describe 'simplib::simp_version' do
 
     context 'rpm query fails' do
       it 'should return unknown' do
-        File.stubs(:read).with('/etc/simp/simp.version').raises(Errno::ENOENT)
-        File.stubs(:read).with(regexp_matches(/metadata.json/), {:encoding => "utf-8"}).returns('')
-        subject.func.stubs(:`).with(rpm_query).returns("package simp is not installed\n")
-        $?.expects(:success?).returns(false)
+        File.stubs(:readable?).with('/etc/simp/simp.version').returns(false)
+        Puppet::Util::Execution.stubs(:execute).with(rpm_query, {:failonfail => true}).raises(Puppet::ExecutionFailure, "Failed")
+#        ({
+#         :exitstatus => 2,
+#         :stdout => "package simp is not installed\n"
+#        })
 
         is_expected.to run.and_return("unknown\n")
       end
