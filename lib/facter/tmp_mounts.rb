5224e1a179f8329eca73bbdea44ad3819a03a266
Trevor Vaughan <tvaughan@onyxpoint.com>
2020-09-04 19:45:23 -0400

    fix code ordering

--- lib/facter/tmp_mounts.rb
+++ lib/facter/tmp_mounts.rb
@@ -22,24 +22,24 @@ simplib__tmp_mount_list = nil
 simplib__tmp_mount_target_dirs.each do |dir|
   Facter.add("tmp_mount#{dir.gsub('/','_')}") do
     setcode do
-      next unless simplib__tmp_mount_list[dir]
       simplib__tmp_mount_list ||= Facter.value(:simplib__mountpoints)
+      next unless simplib__tmp_mount_list[dir]
       simplib__tmp_mount_list[dir]['options'].join(',')
     end
   end
 
   Facter.add("tmp_mount_path#{dir.gsub('/','_')}") do
     setcode do
-      next unless simplib__tmp_mount_list[dir]
       simplib__tmp_mount_list ||= Facter.value(:simplib__mountpoints)
+      next unless simplib__tmp_mount_list[dir]
       simplib__tmp_mount_list[dir]['device']
     end
   end
 
   Facter.add("tmp_mount_fstype#{dir.gsub('/','_')}") do
     setcode do
-      next unless simplib__tmp_mount_list[dir]
       simplib__tmp_mount_list ||= Facter.value(:simplib__mountpoints)
+      next unless simplib__tmp_mount_list[dir]
       simplib__tmp_mount_list[dir]['filesystem']
     end
   end
