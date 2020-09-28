cc968cef8fb1eecb7c1de0e558ee14c6593d948f
Trevor Vaughan <tvaughan@onyxpoint.com>
2020-09-28 19:26:49 -0400

    code cleanup and spec test fixup

--- lib/facter/simplib__numa.rb
+++ lib/facter/simplib__numa.rb
@@ -1,9 +1,9 @@
 # _Description_
 #
-# Return a structured NUMA fact with possible nodes, offline nodes
+# Return a structured NUMA fact with possible nodes, online nodes
 # and a memory mapping layout
 #
-Facter.add("simplib__numa") do
+Facter.add('simplib__numa') do
   confine :kernel => 'Linux'
   confine { File.exist? '/sys/devices/system/node' }
 
@@ -15,24 +15,24 @@ Facter.add("simplib__numa") do
       result['possible'] = File.read('/sys/devices/system/node/possible').strip
     end
 
-    # read the 'offline' nodes file
-    if File.exist?('/sys/devices/system/node/offline')
-      result['offline'] = File.read('/sys/devices/system/node/offline').strip
+    # read the 'online' nodes file
+    if File.exist?('/sys/devices/system/node/online')
+      result['online'] = File.read('/sys/devices/system/node/online').strip
     end
 
+    require 'pathname'
     Dir.glob('/sys/devices/system/node/node*').each do | file |
-      begin
-        File.foreach(file + '/meminfo') do | text |
-          if text =~ / MemTotal:/
-            kb = text.delete("^0-9")
-            bytes = kb.to_i * 1024
-            result[File.basename(file)]['MemTotalBytes'] = bytes
-          end
+      meminfo_file = Pathname.new(File.join(file, 'meminfo'))
+      next unless meminfo_file.exist?
+
+      File.foreach(meminfo_file) do | text |
+        if text =~ /\sMemTotal:\s+(\d+)/
+          result[File.basename(file)] ||= {}
+          result[File.basename(file)]['MemTotalBytes'] = ($1.to_i * 1024)
         end
       end
     end
 
     result
   end
-
 end
