81e3728f55d7451a7df30754b453c229b025b6f5
Trevor Vaughan <tvaughan@onyxpoint.com>
2021-04-29 12:12:55 -0400

    restore spec_helper_acceptance

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,10 +1,15 @@
-# frozen_string_literal: true
-
 require 'beaker-rspec'
 require 'tmpdir'
 require 'yaml'
 require 'simp/beaker_helpers'
 include Simp::BeakerHelpers
+
+require 'beaker/puppet_install_helper'
+require 'beaker-windows'
+include BeakerWindows::Path
+include BeakerWindows::Powershell
+include BeakerWindows::Registry
+include BeakerWindows::WindowsFeature
 
 unless ENV['BEAKER_provision'] == 'no'
   hosts.each do |host|
@@ -17,9 +22,40 @@ unless ENV['BEAKER_provision'] == 'no'
   end
 end
 
+hosts.each do |host|
+  # https://petersouter.co.uk/testing-windows-puppet-with-beaker/
+  case host['platform']
+  when /windows/
+    GEOTRUST_GLOBAL_CA = <<-EOM.freeze
+-----BEGIN CERTIFICATE-----
+MIIDVDCCAjygAwIBAgIDAjRWMA0GCSqGSIb3DQEBBQUAMEIxCzAJBgNVBAYTAlVT
+MRYwFAYDVQQKEw1HZW9UcnVzdCBJbmMuMRswGQYDVQQDExJHZW9UcnVzdCBHbG9i
+YWwgQ0EwHhcNMDIwNTIxMDQwMDAwWhcNMjIwNTIxMDQwMDAwWjBCMQswCQYDVQQG
+EwJVUzEWMBQGA1UEChMNR2VvVHJ1c3QgSW5jLjEbMBkGA1UEAxMSR2VvVHJ1c3Qg
+R2xvYmFsIENBMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2swYYzD9
+9BcjGlZ+W988bDjkcbd4kdS8odhM+KhDtgPpTSEHCIjaWC9mOSm9BXiLnTjoBbdq
+fnGk5sRgprDvgOSJKA+eJdbtg/OtppHHmMlCGDUUna2YRpIuT8rxh0PBFpVXLVDv
+iS2Aelet8u5fa9IAjbkU+BQVNdnARqN7csiRv8lVK83Qlz6cJmTM386DGXHKTubU
+1XupGc1V3sjs0l44U+VcT4wt/lAjNvxm5suOpDkZALeVAjmRCw7+OC7RHQWa9k0+
+bw8HHa8sHo9gOeL6NlMTOdReJivbPagUvTLrGAMoUgRx5aszPeE4uwc2hGKceeoW
+MPRfwCvocWvk+QIDAQABo1MwUTAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBTA
+ephojYn7qwVkDBF9qn1luMrMTjAfBgNVHSMEGDAWgBTAephojYn7qwVkDBF9qn1l
+uMrMTjANBgkqhkiG9w0BAQUFAAOCAQEANeMpauUvXVSOKVCUn5kaFOSPeCpilKIn
+Z57QzxpeR+nBsqTP3UEaBU6bS+5Kb1VSsyShNwrrZHYqLizz/Tt1kL/6cdjHPTfS
+tQWVYrmm3ok9Nns4d0iXrKYgjy6myQzCsplFAMfOEVEiIuCl6rYVSAlk6l5PdPcF
+PseKUgzbFbS9bZvlxrFUaKnjaZC2mqUPuLk/IH2uSrW4nOQdtqvmlKXBx4Ot2/Un
+hw4EbNX/3aBd7YdStysVAq45pmp06drE57xNNB6pXE0zX5IJL4hmXXeXxx12E6nV
+5fEWCRE11azbJHFwLJhWC9kXtNHjUStedejV0NxPNO3CBWaAocvmMw==
+-----END CERTIFICATE-----
+    EOM
+    install_cert_on_windows(host, 'geotrustglobal', GEOTRUST_GLOBAL_CA)
+  end
+end
+
+
 RSpec.configure do |c|
   # ensure that environment OS is ready on each host
-  fix_errata_on hosts
+  fix_errata_on(hosts)
 
   # Readable test descriptions
   c.formatter = :documentation
@@ -27,29 +63,29 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     begin
+      nonwin = hosts.dup
+      nonwin.delete_if {|h| h[:platform] =~ /windows/ }
       # Install modules and dependencies from spec/fixtures/modules
-      copy_fixture_modules_to(hosts)
+      copy_fixture_modules_to( nonwin )
       begin
-        server = only_host_with_role(hosts, 'server')
+        server = only_host_with_role(nonwin, 'server')
       rescue ArgumentError => e
-        server = only_host_with_role(hosts, 'default')
+        server = only_host_with_role(nonwin, 'default')
       end
-
       # Generate and install PKI certificates on each SUT
       Dir.mktmpdir do |cert_dir|
-        run_fake_pki_ca_on(server, hosts, cert_dir)
-        hosts.each { |sut| copy_pki_to(sut, cert_dir, '/etc/pki/simp-testing') }
+        run_fake_pki_ca_on(server, nonwin, cert_dir )
+        nonwin.each{ |sut| copy_pki_to( sut, cert_dir, '/etc/pki/simp-testing' )}
       end
 
       # add PKI keys
       copy_keydist_to(server)
     rescue StandardError, ScriptError => e
-      raise e unless ENV['PRY']
-
-      # rubocop:disable Lint/Debugger
-      require 'pry'
-      binding.pry
-      # rubocop:enable Lint/Debugger
+      if ENV['PRY']
+        require 'pry'; binding.pry
+      else
+        raise e
+      end
     end
   end
 end
