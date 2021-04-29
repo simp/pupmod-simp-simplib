e2ee5a206bb8e5e6b590fb24331f02fff670001a
Trevor Vaughan <tvaughan@onyxpoint.com>
2021-04-29 09:33:50 -0400

    (SIMP-9427) Add simplib::params2hash()
    Added:
      - Add a `simplib::params2hash()` function to return all of the calling scope's
        parameters as a Hash
    
    SIMP-9427 #comment added simplib::params2hash() to make sssd management easier

---
+++ spec/functions/simplib/params2hash_spec.rb
@@ -0,0 +1,119 @@
+require 'spec_helper'
+
+describe 'simplib::params2hash' do
+  let(:pre_condition) {%{
+    class foo(
+      $param1 = 'foo',
+      $prune_me = 'bar',
+      $array = ['one', 'two'],
+      $hash = { 'key' => 'value' }
+    ){
+      $random_var = 'should be ignored'
+
+      notify { 'test': message => 'this is a test'}
+
+      $_params = simplib::params2hash()
+
+      notify { 'class_json': message => $_params.to_json }
+    }
+
+    include 'foo'
+
+    define bar (
+      $param1 = 'foo',
+      $prune_me = 'bar',
+      $array = ['one', 'two'],
+      $hash = { 'key' => 'value' }
+    ){
+      notify { 'test2': message => 'this is another test'}
+
+      $_params = simplib::params2hash()
+
+      notify { 'define_json': message => $_params.to_json }
+    }
+
+    bar{ 'baz': }
+  }}
+
+  it {
+    class_output = JSON.load(catalogue.resource('Notify[class_json]')[:message])
+
+    expect(class_output).to match(
+      {
+        'param1'   => 'foo',
+        'prune_me' => 'bar',
+        'array'    => ['one', 'two'],
+        'hash'     => {'key' => 'value'}
+      }
+    )
+
+    define_output = JSON.load(catalogue.resource('Notify[define_json]')[:message])
+
+    expect(define_output).to match(
+      {
+        'name'     => 'baz',
+        'param1'   => 'foo',
+        'prune_me' => 'bar',
+        'array'    => ['one', 'two'],
+        'hash'     => {'key' => 'value'}
+      }
+    )
+  }
+
+  context 'when pruning values' do
+    let(:pre_condition) {%{
+      class foo(
+        $param1 = 'foo',
+        $prune_me = 'bar',
+        $array = ['one', 'two'],
+        $hash = { 'key' => 'value' }
+      ){
+        notify { 'test': message => 'this is a test'}
+
+        $_params = simplib::params2hash(['prune_me'])
+
+        notify { 'class_json': message => $_params.to_json }
+      }
+
+      include 'foo'
+
+      define bar (
+        $param1 = 'foo',
+        $prune_me = 'bar',
+        $array = ['one', 'two'],
+        $hash = { 'key' => 'value' }
+      ){
+        notify { 'test2': message => 'this is another test'}
+
+        $_params = simplib::params2hash(['prune_me'])
+
+        notify { 'define_json': message => $_params.to_json }
+      }
+
+      bar{ 'baz': }
+    }}
+
+    it {
+      class_output = JSON.load(catalogue.resource('Notify[class_json]')[:message])
+
+      expect(class_output).to match(
+        {
+          'param1'   => 'foo',
+          'array'    => ['one', 'two'],
+          'hash'     => {'key' => 'value'}
+        }
+      )
+
+      define_output = JSON.load(catalogue.resource('Notify[define_json]')[:message])
+
+      expect(define_output).to match(
+        {
+          'name'     => 'baz',
+          'param1'   => 'foo',
+          'array'    => ['one', 'two'],
+          'hash'     => {'key' => 'value'}
+        }
+      )
+    }
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet'
+require 'super_diff/rspec'
 require 'simp/rspec-puppet-facts'
 include Simp::RspecPuppetFacts
 
@@ -7,19 +10,21 @@ require 'pathname'
 
 # RSpec Material
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
-module_name = File.basename(File.expand_path(File.join(__FILE__,'../..')))
+module_name = File.basename(File.expand_path(File.join(__FILE__, '../..')))
 
-if !ENV.key?( 'TRUSTED_NODE_DATA' )
-  warn '== WARNING: TRUSTED_NODE_DATA is unset, using TRUSTED_NODE_DATA=yes'
-  ENV['TRUSTED_NODE_DATA']='yes'
+if ENV['PUPPET_DEBUG']
+  Puppet::Util::Log.level = :debug
+  Puppet::Util::Log.newdestination(:console)
 end
 
-default_hiera_config =<<-EOM
+default_hiera_config = <<~HIERA_CONFIG
 ---
 version: 5
 hierarchy:
   - name: SIMP Compliance Engine
     lookup_key: compliance_markup::enforcement
+    options:
+      enabled_sce_versions: [2]
   - name: Custom Test Hiera
     path: "%{custom_hiera}.yaml"
   - name: "%{module_name}"
@@ -29,7 +34,7 @@ hierarchy:
 defaults:
   data_hash: yaml_data
   datadir: "stub"
-EOM
+HIERA_CONFIG
 
 # This can be used from inside your spec tests to set the testable environment.
 # You can use this to stub out an ENC.
@@ -67,26 +72,23 @@ def set_hieradata(hieradata)
   RSpec.configure { |c| c.default_facts['custom_hiera'] = hieradata }
 end
 
-if not File.directory?(File.join(fixture_path,'hieradata')) then
-  FileUtils.mkdir_p(File.join(fixture_path,'hieradata'))
+unless File.directory?(File.join(fixture_path, 'hieradata'))
+  FileUtils.mkdir_p(File.join(fixture_path, 'hieradata'))
 end
 
-if not File.directory?(File.join(fixture_path,'modules',module_name)) then
-  FileUtils.mkdir_p(File.join(fixture_path,'modules',module_name))
+unless File.directory?(File.join(fixture_path, 'modules', module_name))
+  FileUtils.mkdir_p(File.join(fixture_path, 'modules', module_name))
 end
 
 RSpec.configure do |c|
   # If nothing else...
   c.default_facts = {
-    :production => {
+    production: {
       #:fqdn           => 'production.rspec.test.localdomain',
-      :path           => '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin',
-      :concat_basedir => '/tmp'
+      path: '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin',
+      concat_basedir: '/tmp'
     }
   }
-
-#FIXME Windows tests fail when enabled
-#  c.trusted_server_facts = true
 
   c.mock_framework = :rspec
   c.mock_with :rspec
@@ -94,12 +96,12 @@ RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
 
-  c.hiera_config = File.join(fixture_path,'hieradata','hiera.yaml')
+  c.hiera_config = File.join(fixture_path, 'hieradata', 'hiera.yaml')
 
   # Useless backtrace noise
   backtrace_exclusion_patterns = [
-    /spec_helper/,
-    /gems/
+    %r{spec_helper},
+    %r{gems},
   ]
 
   if c.respond_to?(:backtrace_exclusion_patterns)
@@ -108,9 +110,10 @@ RSpec.configure do |c|
     c.backtrace_clean_patterns = backtrace_exclusion_patterns
   end
 
+  # rubocop:disable RSpec/BeforeAfterAll
   c.before(:all) do
-    data = YAML.load(default_hiera_config)
-    data.keys.each do |key|
+    data = YAML.safe_load(default_hiera_config)
+    data.each_key do |key|
       next unless data[key].is_a?(Hash)
 
       if data[key][:datadir] == 'stub'
@@ -124,31 +127,33 @@ RSpec.configure do |c|
       f.write data.to_yaml
     end
   end
+  # rubocop:enable RSpec/BeforeAfterAll
 
   c.before(:each) do
     @spec_global_env_temp = Dir.mktmpdir('simpspec')
 
     if defined?(environment)
       set_environment(environment)
-      FileUtils.mkdir_p(File.join(@spec_global_env_temp,environment.to_s))
+      FileUtils.mkdir_p(File.join(@spec_global_env_temp, environment.to_s))
     end
 
     # ensure the user running these tests has an accessible environmentpath
+    Puppet[:digest_algorithm] = 'sha256'
     Puppet[:environmentpath] = @spec_global_env_temp
     Puppet[:user] = Etc.getpwuid(Process.uid).name
     Puppet[:group] = Etc.getgrgid(Process.gid).name
 
     # sanitize hieradata
     if defined?(hieradata)
-      set_hieradata(hieradata.gsub(':','_'))
+      set_hieradata(hieradata.gsub(':', '_'))
     elsif defined?(class_name)
-      set_hieradata(class_name.gsub(':','_'))
+      set_hieradata(class_name.gsub(':', '_'))
     end
   end
 
   c.after(:each) do
     # clean up the mocked environmentpath
-    FileUtils.remove_entry_secure(@spec_global_env_temp)
+    FileUtils.rm_rf(@spec_global_env_temp)
     @spec_global_env_temp = nil
   end
 end
@@ -156,12 +161,7 @@ end
 Dir.glob("#{RSpec.configuration.module_path}/*").each do |dir|
   begin
     Pathname.new(dir).realpath
-  rescue
-    fail "ERROR: The module '#{dir}' is not installed. Tests cannot continue."
+  rescue StandardError
+    raise "ERROR: The module '#{dir}' is not installed. Tests cannot continue."
   end
-end
-
-if ENV['PUPPET_DEBUG']
-  Puppet::Util::Log.level = :debug
-  Puppet::Util::Log.newdestination(:console)
 end
