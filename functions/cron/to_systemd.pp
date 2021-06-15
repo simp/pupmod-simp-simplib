889becb0878306f308c4121e0c9428635d306e67
Trevor Vaughan <tvaughan@onyxpoint.com>
2021-06-15 17:10:51 -0400

    (SIMP-9630) Fix systemd cron conversion
    * Fixed
      * Add support for multi-item entries
    
    SIMP-9630 #comment fixed systemd cron conversion

--- functions/cron/to_systemd.pp
+++ functions/cron/to_systemd.pp
@@ -25,40 +25,55 @@ function simplib::cron::to_systemd(
   Simplib::Cron::Monthday          $monthday = '*',
   Optional[Simplib::Cron::Weekday] $weekday  = undef
 ) {
-  $_month = $month ? {
-    /(?i:jan)/ => 1,
-    /(?i:feb)/ => 2,
-    /(?i:mar)/ => 3,
-    /(?i:apr)/ => 4,
-    /(?i:may)/ => 5,
-    /(?i:jun)/ => 6,
-    /(?i:jul)/ => 7,
-    /(?i:aug)/ => 8,
-    /(?i:sep)/ => 9,
-    /(?i:oct)/ => 10,
-    /(?i:nov)/ => 11,
-    /(?i:dec)/ => 12,
-    default    => $month
-  }
+  $_minute = Array($minute, true).join(',')
 
-  $_weekday = $weekday ? {
-    '*'     => undef,
-    0       => 'Sun',
-    1       => 'Mon',
-    2       => 'Tue',
-    3       => 'Wed',
-    4       => 'Thu',
-    5       => 'Fri',
-    6       => 'Sat',
-    default => $weekday
+  $_hour = Array($hour, true).join(',')
+
+  $_month = Array($month, true).map |$m| {
+    $m ? {
+      /(?i:jan)/ => 1,
+      /(?i:feb)/ => 2,
+      /(?i:mar)/ => 3,
+      /(?i:apr)/ => 4,
+      /(?i:may)/ => 5,
+      /(?i:jun)/ => 6,
+      /(?i:jul)/ => 7,
+      /(?i:aug)/ => 8,
+      /(?i:sep)/ => 9,
+      /(?i:oct)/ => 10,
+      /(?i:nov)/ => 11,
+      /(?i:dec)/ => 12,
+      default    => $m
+    }
+  }.join(',')
+
+  $_monthday = Array($monthday, true).join(',')
+
+  if $weekday {
+    $_weekday = Array($weekday, true).map |$w| {
+      $w ? {
+        '*'     => undef,
+        0       => 'Sun',
+        1       => 'Mon',
+        2       => 'Tue',
+        3       => 'Wed',
+        4       => 'Thu',
+        5       => 'Fri',
+        6       => 'Sat',
+        default => $weekday
+      }
+    }.join(',')
+  }
+  else {
+    $_weekday = ''
   }
 
   strip(
     join(
       [
         $_weekday,
-        join([$_month, $monthday], '-'),
-        join([$hour, $minute], ':')
+        join([$_month, $_monthday], '-'),
+        join([$_hour, $_minute], ':')
       ],
       ' '
     )
