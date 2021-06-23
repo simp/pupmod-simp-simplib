b5c1384b12b5e517ec7d0bb48b10d019fd537028
Trevor Vaughan <tvaughan@onyxpoint.com>
2021-06-23 10:49:09 -0400

    (SIMP-8955) Fix simplib__networkmanager fact (#256)
    * Also fixed the version number
    
    SIMP-8955 #close

--- spec/unit/facter/simplib__networkmanager_spec.rb
+++ spec/unit/facter/simplib__networkmanager_spec.rb
@@ -7,40 +7,42 @@ describe 'simplib__networkmanager' do
     expect(Facter::Core::Execution).to receive(:exec).with('uname -s').and_return('Linux')
     expect(Facter::Util::Resolution).to receive(:which).with('nmcli').and_return('/usr/sbin/nmcli')
 
-    expect(Facter::Core::Execution).to receive(:execute).with('/usr/sbin/nmcli -t -m multiline general status', :on_fail => :failed).and_return(general_status)
-    expect(Facter::Core::Execution).to receive(:execute).with('/usr/sbin/nmcli -t general hostname', :on_fail => :failed).and_return(general_hostname)
-    expect(Facter::Core::Execution).to receive(:execute).with('/usr/sbin/nmcli -t connection show', :on_fail => :failed).and_return(connections)
+    expect(Facter::Core::Execution).to receive(:execute).with('/usr/sbin/nmcli -t -m multiline general status').and_return(general_status)
+    expect(Facter::Core::Execution).to receive(:execute).with('/usr/sbin/nmcli -t general hostname').and_return(general_hostname)
+    expect(Facter::Core::Execution).to receive(:execute).with('/usr/sbin/nmcli -t connection show').and_return(connections)
   end
 
   context 'nmcli fails' do
-    let(:general_status){ :failed }
-    let(:general_hostname){ :failed }
-    let(:connections){ :failed }
+    let(:general_status){ '' }
+    let(:general_hostname){ '' }
+    let(:connections){ '' }
 
     it 'returns "enabled" = false' do
+      allow_any_instance_of(Process::Status).to receive(:success?).and_return(false)
+
       expect(Facter.fact('simplib__networkmanager').value).to eq({'enabled' => false})
     end
   end
 
   context 'nmcli succeeds' do
     let(:general_status){
-      <<-EOM
-STATE:connected
-CONNECTIVITY:full
-WIFI-HW:enabled
-WIFI:enabled
-WWAN-HW:enabled
-WWAN:enabled
-      EOM
+      <<~EOM
+        STATE:connected
+        CONNECTIVITY:full
+        WIFI-HW:enabled
+        WIFI:enabled
+        WWAN-HW:enabled
+        WWAN:enabled
+        EOM
     }
 
     let(:general_hostname){ "foo.bar.baz\n" }
 
     let(:connections){
-      <<-EOM
-Eth Dev:b961cb37-ae05-4c67-98b0-432465fe03c2:802-3-ethernet:eth0
-Bridge Dev:0c190f3f-262b-4585-a7de-2a146896ea86:bridge:virbr0
-      EOM
+      <<~EOM
+        Eth Dev:b961cb37-ae05-4c67-98b0-432465fe03c2:802-3-ethernet:eth0
+        Bridge Dev:0c190f3f-262b-4585-a7de-2a146896ea86:bridge:virbr0
+        EOM
     }
 
     let(:expected){{
